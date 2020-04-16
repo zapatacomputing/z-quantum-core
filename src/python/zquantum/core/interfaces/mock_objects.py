b0f50b711e84541bc4b803838fc32c245087bb00
simonwa7 <simonwa7@gmail.com>
2020-04-16 17:11:21 -0400

    Fixes to mock objects so they align with interfaces

--- src/python/zquantum/core/interfaces/mock_objects.py
+++ src/python/zquantum/core/interfaces/mock_objects.py
@@ -6,26 +6,28 @@ from scipy.optimize import OptimizeResult
 import numpy as np
 
 class MockQuantumSimulator(QuantumSimulator):
+    def __init__(self, n_samples=None):
+        self.n_samples = n_samples
 
-    def run_circuit_and_measure(self, circuit, n_samples, **kwargs):
+    def run_circuit_and_measure(self, circuit, **kwargs):
         n_qubits = len(circuit.qubits)
         measurements = []
-        for _ in range(n_samples):
+        for _ in range(self.n_samples):
             measurements.append(tuple([random.randint(0,1) for j in range(n_qubits)]))
         return measurements
     
-    def get_expectation_values(self, circuit, **kwargs):
+    def get_expectation_values(self, circuit, operator, **kwargs):
         n_qubits = len(circuit.qubits)
         values = [random.random() for i in range(n_qubits)]
         return ExpectationValues(values)
 
-    def get_exact_expectation_values(self, circuit, **kwargs):
+    def get_exact_expectation_values(self, circuit, operator, **kwargs):
         return self.get_expectation_values(circuit)
 
-    def get_wavefunction(self):
+    def get_wavefunction(self, circuit):
         raise NotImplementedError
 
-    def get_density_matrix(self):
+    def get_density_matrix(self, circuit):
         raise NotImplementedError
 
     
@@ -37,5 +39,6 @@ class MockOptimizer(Optimizer):
             new_parameters[i] += random.random()
         new_parameters = np.array(new_parameters)
         result.opt_value = cost_function(new_parameters)
+        result['history'] = [{'value': result.opt_value, 'params': new_parameters}]
         result.opt_params = new_parameters
         return result

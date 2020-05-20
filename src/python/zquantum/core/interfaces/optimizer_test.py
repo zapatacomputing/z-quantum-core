1442db0351272ac31ccf295710fd64c8125be091
simonwa7 <simonwa7@gmail.com>
2020-05-20 16:06:46 -0400

    Adding base unittests for optimizers

---
+++ src/python/zquantum/core/interfaces/optimizer_test.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+
+def rosen(x):
+     """The Rosenbrock function"""
+     return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+
+class OptimizerTests(object):
+    # To run tests with this base class, the following variables need to be properly initialized in the child class:
+        # self.optimizers
+
+    def test_optimization(self):
+        for optimizer in self.optimizers:
+            results = optimizer.minimize(rosen, initial_params=[0, 0])
+            self.assertAlmostEqual(results.opt_value, 0, places=5)
+            self.assertAlmostEqual(results.opt_params[0], 1, places=4)
+            self.assertAlmostEqual(results.opt_params[1], 1, places=4)
+
+            self.assertIn("nfev", results.keys())
+            self.assertIn("nit", results.keys())
+            self.assertIn("opt_value", results.keys())
+            self.assertIn("opt_params", results.keys())
+            self.assertIn("history", results.keys())
+

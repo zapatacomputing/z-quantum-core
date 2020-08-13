e8dd72d43b948b2f08e8982b093cd0bae66a6130
Amara Katabarwa <Henriamaa@gmail.com>
2020-08-13 09:05:03 -0400

    initial commit

---
+++ src/python/zquantum/core/bitstring_distribution/distance_measures/jensen_shannon_divergence.py
@@ -0,0 +1,31 @@
+import math
+from typing import Dict, TYPE_CHECKING
+from .clipped_negative_log_likelihood import compute_clipped_negative_log_likelihood
+if TYPE_CHECKING:
+    from zquantum.core.bitstring_distribution import BitstringDistribution
+
+
+def compute_jensen_shannon_divergence(
+    target_distribution: "BitstringDistribution",
+    measured_distribution: "BitstringDistribution",
+    distance_measure_parameters: Dict,
+) -> float:
+    """Computes the symmetrized version of the clipped negative log likelihood between a target bitstring distribution
+    and a measured bitstring distribution
+    See Equation (4) in https://advances.sciencemag.org/content/5/10/eaaw9918?rss=1
+
+    Args:
+        target_distribution (BitstringDistribution): The target bitstring probability distribution.
+        measured_distribution (BitstringDistribution): The measured bitstring probability distribution.
+
+        distance_measure_parameters (dict):
+            - epsilon (float): The small parameter needed to regularize log computation when argument is zero. The default value is 1e-9.
+
+    Returns:
+        float: The value of the symmetrized version
+    """
+
+    value = compute_clipped_negative_log_likelihood(target_distribution, measured_distribution, distance_measure_parameters)/2 + \
+            compute_clipped_negative_log_likelihood(measured_distribution, target_distribution, distance_measure_parameters)/2
+
+    return value

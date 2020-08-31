387f8aa31108b96e88e8ebb2a4d62ae7c88a735c
Ethan Hansen <1ethanhansen@protonmail.com>
2020-08-31 16:48:38 -0700

    trying to get json from string
    Co-Authored-By: Max Radin <10065071+max-radin@users.noreply.github.com>

--- tasks/circuit_tasks.py
+++ tasks/circuit_tasks.py
@@ -1,9 +1,11 @@
 import numpy as np
+from json import loads
 from zquantum.core.circuit import save_circuit_template_params
 from zquantum.core.utils import create_object
 
 def generate_random_ansatz_params(ansatz_specs, min_value, max_value, seed=None):
     if ansatz_specs is not None:
+        ansatz_specs_dict = loads(ansatz_specs)
         ansatz = create_object(ansatz_specs)
         number_of_params = ansatz.number_of_params
     if seed is not None:

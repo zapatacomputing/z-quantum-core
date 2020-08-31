f73a921a4db61ca3296a250599e626c889a4a104
Ethan Hansen <1ethanhansen@protonmail.com>
2020-08-31 16:34:51 -0700

    put default at the back
    Co-Authored-By: Max Radin <10065071+max-radin@users.noreply.github.com>

--- tasks/circuit_tasks.py
+++ tasks/circuit_tasks.py
@@ -2,8 +2,7 @@ import numpy as np
 from zquantum.core.circuit import save_circuit_template_params
 from zquantum.core.utils import create_object
 
-def generate_random_ansatz_params(ansatz_specs, seed=None, min_value, max_value):
-
+def generate_random_ansatz_params(ansatz_specs, min_value, max_value, seed=None):
     if ansatz_specs is not None:
         ansatz = create_object(ansatz_specs)
         number_of_params = ansatz.number_of_params

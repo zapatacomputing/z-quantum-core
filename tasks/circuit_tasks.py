78cdefcb203e240fa905d8fa041aeb4689624598
Marta Mauri <marta.mauri14@gmail.com>
2020-09-14 13:40:09 +0200

    debugging generate-params

--- tasks/circuit_tasks.py
+++ tasks/circuit_tasks.py
@@ -25,7 +25,7 @@ def generate_random_ansatz_params(
     # number_of_parameters=None,
     min_value=-np.pi * 0.5,
     max_value=np.pi * 0.5,
-    seed="None",
+    seed=None,
 ):
     if ansatz_specs is not None:
         ansatz_specs_dict = loads(ansatz_specs)
@@ -33,8 +33,8 @@ def generate_random_ansatz_params(
         number_of_params = ansatz.number_of_params
     # elif number_of_parameters is not None:
     #     number_of_params = number_of_parameters
-    assert seed == "None"
-    if seed != "None":
+    # assert seed == "None"
+    if seed is not None:
         np.random.seed(seed)
     params = np.random.uniform(min_value, max_value, number_of_params)
     save_circuit_template_params(params, "params.json")

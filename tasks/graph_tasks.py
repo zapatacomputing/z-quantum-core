3a006088f23d5e05f5d3bba893ceeb46e491c6ad
Marta Mauri <marta.mauri14@gmail.com>
2020-09-14 17:35:29 +0200

    adding graph tasks

---
+++ tasks/graph_tasks.py
@@ -0,0 +1,40 @@
+from zquantum.core.graph import (
+    generate_random_graph_erdos_renyi,
+    generate_random_regular_graph,
+    generate_graph_from_specs,
+    save_graph,
+)
+from json import loads
+
+
+def generate_erdos_renyi_graph(
+    number_of_nodes, edge_probability, random_weights=False, seed="None"
+):
+    if seed == "None":
+        seed = None
+    graph = generate_random_graph_erdos_renyi(
+        number_of_nodes, edge_probability, random_weights, seed
+    )
+    save_graph(graph, "graph.json")
+
+
+def generate_regular_graph(number_of_nodes, degree, random_weights=False, seed="None"):
+    if seed == "None":
+        seed = None
+    graph = generate_random_regular_graph(number_of_nodes, degree, random_weights, seed)
+    save_graph(graph, "graph.json")
+
+
+def generate_complete_graph(number_of_nodes, random_weights=False, seed="None"):
+    if seed == "None":
+        seed = None
+    graph = generate_random_graph_erdos_renyi(
+        number_of_nodes, 1.0, random_weights, seed
+    )
+    save_graph(graph, "graph.json")
+
+
+def generate_graph_from_specs(graph_specs):
+    graph_specs_dict = loads(graph_specs)
+    graph = generate_graph_from_specs(graph_specs_dict)
+    save_graph(graph, "graph.json")

170c019af4ccea36b687aa4986f5674d6d470694
Marta Mauri <marta.mauri14@gmail.com>
2020-09-23 10:24:37 -0400

    renaming steps using underscore convention

--- steps/circuit.py
+++ steps/circuit.py
@@ -1,23 +1,23 @@
 import numpy as np
 import json
 from zquantum.core.circuit import (
+    build_circuit_layers_and_connectivity as _build_circuit_layers_and_connectivity,
+    add_ancilla_register_to_circuit as _add_ancilla_register_to_circuit,
+    combine_ansatz_params as _combine_ansatz_params,
+    build_uniform_param_grid as _build_uniform_param_grid,
     save_circuit_template_params,
-    combine_ansatz_params,
     load_circuit_template_params,
     save_circuit,
     save_parameter_grid,
-    build_uniform_param_grid,
     save_circuit_layers,
     save_circuit_connectivity,
-    build_circuit_layers_and_connectivity,
     load_circuit,
-    add_ancilla_register_to_circuit,
     load_circuit_set,
     Circuit,
     save_circuit_set,
 )
 from zquantum.core.utils import create_object
-from zquantum.core.testing import create_random_circuit
+from zquantum.core.testing import create_random_circuit as _create_random_circuit
 
 # Generate random parameters for an ansatz
 def generate_random_ansatz_params(
@@ -40,10 +40,10 @@ def generate_random_ansatz_params(
 
 
 # Combine two sets of ansatz parameters
-def combine_ansatz_parameters(params1, params2):
+def combine_ansatz_params(params1, params2):
     parameters1 = load_circuit_template_params(params1)
     parameters2 = load_circuit_template_params(params2)
-    combined_params = combine_ansatz_params(parameters1, parameters2)
+    combined_params = _combine_ansatz_params(parameters1, parameters2)
     save_circuit_template_params(combined_params, "combined-params.json")
 
 
@@ -65,7 +65,7 @@ def build_ansatz_circuit(ansatz_specs, params="None"):
 
 
 # Build uniform parameter grid
-def build_uniform_parameter_grid(
+def build_uniform_param_grid(
     ansatz_specs,
     number_of_params_per_layer="None",
     number_of_layers=1,
@@ -79,18 +79,18 @@ def build_uniform_parameter_grid(
     elif number_of_params_per_layer != "None":
         number_of_params = number_of_params_per_layer
 
-    grid = build_uniform_param_grid(
+    grid = _build_uniform_param_grid(
         number_of_params, number_of_layers, min_value, max_value, step
     )
     save_parameter_grid(grid, "parameter-grid.json")
 
 
 # Build circuit layers and connectivity
-def build_layers_and_connectivity(
+def build_circuit_layers_and_connectivity(
     x_dimension, y_dimension="None", layer_type="nearest-neighbor"
 ):
     # TODO None issue in workflow v1
-    connectivity, layers = build_circuit_layers_and_connectivity(
+    connectivity, layers = _build_circuit_layers_and_connectivity(
         x_dimension, y_dimension, layer_type
     )
     save_circuit_layers(layers, "circuit-layers.json")
@@ -98,15 +98,15 @@ def build_layers_and_connectivity(
 
 
 # Create random circuit
-def build_random_circuit(number_of_qubits, number_of_gates, seed="None"):
-    circuit = create_random_circuit(number_of_qubits, number_of_gates, seed=seed)
+def create_random_circuit(number_of_qubits, number_of_gates, seed="None"):
+    circuit = _create_random_circuit(number_of_qubits, number_of_gates, seed=seed)
     save_circuit(circuit, "circuit.json")
 
 
 # Add register of ancilla qubits to circuit
-def add_ancilla_qubits_register_to_circuit(number_of_ancilla_qubits, circuit):
+def add_ancilla_register_to_circuit(number_of_ancilla_qubits, circuit):
     circuit_object = load_circuit(circuit)
-    extended_circuit = add_ancilla_register_to_circuit(
+    extended_circuit = _add_ancilla_register_to_circuit(
         circuit_object, number_of_ancilla_qubits
     )
     save_circuit(extended_circuit, "extended-circuit.json")

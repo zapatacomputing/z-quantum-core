5b18e8c35d86e68836dfcf3cf284a1cd7eeabf6e
simonwa7 <simonwa7@gmail.com>
2020-09-28 16:41:48 -0400

    Adding several single qubit gates and tests

--- src/python/zquantum/core/circuit/gates/single_qubit_gates.py
+++ src/python/zquantum/core/circuit/gates/single_qubit_gates.py
@@ -26,7 +26,7 @@ class Y(Gate):
 
     def __init__(self, qubit: int):
         matrix = sympy.Matrix(
-            [[complex(0, 0), complex(0, -1)], [complex(1, 0), complex(0, 0)]]
+            [[complex(0, 0), complex(0, -1)], [complex(0, 1), complex(0, 0)]]
         )
         super().__init__(matrix=matrix, qubits=(qubit,))
 
@@ -72,25 +72,119 @@ class I(Gate):
         super().__init__(matrix=matrix, qubits=(qubit,))
 
 
+class Phase(Gate):
+    """Quantum Phase gate
+
+    Inputs:
+        qubits (tuple[int]): A list of qubit indices that the operator acts on
+        parameter (Union[float, sympy.Symbol]): The value of the parameter used in the Phase Gate. If a float is
+            passed, the returned Gate is evaluated to the specified parameter value
+    """
+
+    def __init__(self, qubit: int):
+        matrix = sympy.Matrix(
+            [
+                [1, 0],
+                [0, complex(0, 1)],
+            ]
+        )
+        super().__init__(matrix=matrix, qubits=(qubit,))
+
+
+class T(Gate):
+    """Quantum T gate
+
+    Inputs:
+        qubits (tuple[int]): A list of qubit indices that the operator acts on
+        parameter (Union[float, sympy.Symbol]): The value of the parameter used in the T Gate. If a float is
+            passed, the returned Gate is evaluated to the specified parameter value
+    """
+
+    def __init__(self, qubit: int):
+        matrix = sympy.Matrix(
+            [
+                [1, 0],
+                [0, sympy.exp(complex(0, np.pi / 4))],
+            ]
+        )
+        super().__init__(matrix=matrix, qubits=(qubit,))
+
+
 class Rx(Gate):
     """Quantum Rx gate
 
     Inputs:
-        qubits (tuple[int]): A list of qubit indices that the operator acts on 
+        qubits (tuple[int]): A list of qubit indices that the operator acts on
         parameter (Union[float, sympy.Symbol]): The value of the parameter used in the Rx Gate. If a float is
             passed, the returned Gate is evaluated to the specified parameter value
     """
 
-    def __init__(self, qubit: int, parameter: Union[float, sympy.Symbol] = "theta"):
+    def __init__(
+        self, qubit: int, parameter: Union[float, sympy.Symbol] = sympy.Symbol("theta")
+    ):
         matrix = sympy.Matrix(
             [
                 [
-                    complex(sympy.cos(parameter / 2), 0),
-                    complex(0, -1 * sympy.sin(parameter / 2)),
+                    sympy.cos(parameter / 2),
+                    -sympy.I * sympy.sin(parameter / 2),
                 ],
                 [
-                    complex(0, -1 * sympy.sin(parameter / 2)),
-                    complex(sympy.cos(parameter / 2), 0),
+                    -sympy.I * sympy.sin(parameter / 2),
+                    sympy.cos(parameter / 2),
+                ],
+            ]
+        )
+        super().__init__(matrix=matrix, qubits=(qubit,))
+
+
+class Ry(Gate):
+    """Quantum Ry gate
+
+    Inputs:
+        qubits (tuple[int]): A list of qubit indices that the operator acts on
+        parameter (Union[float, sympy.Symbol]): The value of the parameter used in the Ry Gate. If a float is
+            passed, the returned Gate is evaluated to the specified parameter value
+    """
+
+    def __init__(
+        self, qubit: int, parameter: Union[float, sympy.Symbol] = sympy.Symbol("theta")
+    ):
+        matrix = sympy.Matrix(
+            [
+                [
+                    sympy.cos(parameter / 2),
+                    -1 * sympy.sin(parameter / 2),
+                ],
+                [
+                    sympy.sin(parameter / 2),
+                    sympy.cos(parameter / 2),
+                ],
+            ]
+        )
+        super().__init__(matrix=matrix, qubits=(qubit,))
+
+
+class Rz(Gate):
+    """Quantum Rz gate
+
+    Inputs:
+        qubits (tuple[int]): A list of qubit indices that the operator acts on
+        parameter (Union[float, sympy.Symbol]): The value of the parameter used in the Rz Gate. If a float is
+            passed, the returned Gate is evaluated to the specified parameter value
+    """
+
+    def __init__(
+        self, qubit: int, parameter: Union[float, sympy.Symbol] = sympy.Symbol("theta")
+    ):
+        matrix = sympy.Matrix(
+            [
+                [
+                    sympy.exp(-1 * sympy.I * parameter / 2),
+                    0,
+                ],
+                [
+                    0,
+                    sympy.exp(sympy.I * parameter / 2),
                 ],
             ]
         )
--- src/python/zquantum/core/circuit/gates/single_qubit_gates_test.py
+++ src/python/zquantum/core/circuit/gates/single_qubit_gates_test.py
@@ -1,8 +1,240 @@
-from .single_qubit_gates import X
+from .single_qubit_gates import X, Y, Z, H, I, Phase, T, Rx, Ry, Rz
+from ._gate import Gate
 import pytest
+import numpy as np
 import sympy
 
 
-def test_creating_X_gate():
-    """The Gate class should raise an assertion error if the matrix is not square"""
-    gate = X(1)
+@pytest.mark.parametrize(
+    "qubit",
+    [0, 1, 2, 3, 4, 5, 10, 11, 12, 99, 100, 101],
+)
+def test_creating_X_gate(qubit):
+    gate = X(qubit)
+    Xgate = Gate(sympy.Matrix([[0, 1], [1, 0]]), (qubit,))
+    assert gate == Xgate
+
+
+@pytest.mark.parametrize(
+    "qubit",
+    [0, 1, 2, 3, 4, 5, 10, 11, 12, 99, 100, 101],
+)
+def test_creating_Y_gate(qubit):
+    gate = Y(qubit)
+    Ygate = Gate(
+        sympy.Matrix([[complex(0, 0), complex(0, -1)], [complex(0, 1), complex(0, 0)]]),
+        (qubit,),
+    )
+    assert gate == Ygate
+
+
+@pytest.mark.parametrize(
+    "qubit",
+    [0, 1, 2, 3, 4, 5, 10, 11, 12, 99, 100, 101],
+)
+def test_creating_Z_gate(qubit):
+    gate = Z(qubit)
+    Zgate = Gate(
+        sympy.Matrix([[1, 0], [0, -1]]),
+        (qubit,),
+    )
+    assert gate == Zgate
+
+
+@pytest.mark.parametrize(
+    "qubit",
+    [0, 1, 2, 3, 4, 5, 10, 11, 12, 99, 100, 101],
+)
+def test_creating_H_gate(qubit):
+    gate = H(qubit)
+    Hgate = Gate(
+        sympy.Matrix(
+            [
+                [(1 / np.sqrt(2)), (1 / np.sqrt(2))],
+                [(1 / np.sqrt(2)), -1 * (1 / np.sqrt(2))],
+            ]
+        ),
+        (qubit,),
+    )
+    assert gate == Hgate
+
+
+@pytest.mark.parametrize(
+    "qubit",
+    [0, 1, 2, 3, 4, 5, 10, 11, 12, 99, 100, 101],
+)
+def test_creating_I_gate(qubit):
+    gate = I(qubit)
+    Igate = Gate(
+        sympy.Matrix(
+            [
+                [1, 0],
+                [0, 1],
+            ]
+        ),
+        (qubit,),
+    )
+    assert gate == Igate
+
+
+@pytest.mark.parametrize(
+    "qubit",
+    [0, 1, 2, 3, 4, 5, 10, 11, 12, 99, 100, 101],
+)
+def test_creating_Phase_gate(qubit):
+    gate = Phase(qubit)
+    Phasegate = Gate(
+        sympy.Matrix(
+            [
+                [1, 0],
+                [0, complex(0, 1)],
+            ]
+        ),
+        (qubit,),
+    )
+    assert gate == Phasegate
+
+
+@pytest.mark.parametrize(
+    "qubit",
+    [0, 1, 2, 3, 4, 5, 10, 11, 12, 99, 100, 101],
+)
+def test_creating_T_gate(qubit):
+    gate = T(qubit)
+    Tgate = Gate(
+        sympy.Matrix(
+            [
+                [1, 0],
+                [0, sympy.exp(complex(0, np.pi / 4))],
+            ]
+        ),
+        (qubit,),
+    )
+    assert gate == Tgate
+
+
+rotation_gate_test_data = [
+    [qubit, theta]
+    for qubit in [0, 1, 2, 3, 4, 5, 10, 11, 12, 99, 100, 101]
+    for theta in [
+        -2 * np.pi,
+        -np.pi,
+        -np.pi / 2,
+        0,
+        1,
+        np.pi / 2,
+        np.pi,
+        2 * np.pi,
+        1.012,
+        -0.0001,
+        sympy.Symbol("theta"),
+        sympy.Symbol("beta"),
+        sympy.Symbol("Gamma"),
+        None,
+    ]
+]
+
+
+@pytest.mark.parametrize("qubit, theta", rotation_gate_test_data)
+def test_creating_Rx_gate(qubit, theta):
+    if theta is not None:
+        gate = Rx(qubit, theta)
+        Rxgate = Gate(
+            sympy.Matrix(
+                [
+                    [
+                        sympy.cos(theta / 2),
+                        -sympy.I * sympy.sin(theta / 2),
+                    ],
+                    [
+                        -sympy.I * sympy.sin(theta / 2),
+                        sympy.cos(theta / 2),
+                    ],
+                ]
+            ),
+            (qubit,),
+        )
+    else:
+        gate = Rx(qubit)
+        Rxgate = Gate(
+            sympy.Matrix(
+                [
+                    [
+                        sympy.cos(sympy.Symbol("theta") / 2),
+                        -sympy.I * sympy.sin(sympy.Symbol("theta") / 2),
+                    ],
+                    [
+                        -sympy.I * sympy.sin(sympy.Symbol("theta") / 2),
+                        sympy.cos(sympy.Symbol("theta") / 2),
+                    ],
+                ]
+            ),
+            (qubit,),
+        )
+    assert gate == Rxgate
+
+
+@pytest.mark.parametrize("qubit, theta", rotation_gate_test_data)
+def test_creating_Ry_gate(qubit, theta):
+    if theta is not None:
+        gate = Ry(qubit, theta)
+        Rygate = Gate(
+            sympy.Matrix(
+                [
+                    [
+                        sympy.cos(theta / 2),
+                        -1 * sympy.sin(theta / 2),
+                    ],
+                    [
+                        sympy.sin(theta / 2),
+                        sympy.cos(theta / 2),
+                    ],
+                ]
+            ),
+            (qubit,),
+        )
+    else:
+        gate = Ry(qubit)
+        Rygate = Gate(
+            sympy.Matrix(
+                [
+                    [
+                        sympy.cos(sympy.Symbol("theta") / 2),
+                        -1 * sympy.sin(sympy.Symbol("theta") / 2),
+                    ],
+                    [
+                        sympy.sin(sympy.Symbol("theta") / 2),
+                        sympy.cos(sympy.Symbol("theta") / 2),
+                    ],
+                ]
+            ),
+            (qubit,),
+        )
+    assert gate == Rygate
+
+
+@pytest.mark.parametrize("qubit, theta", rotation_gate_test_data)
+def test_creating_Rz_gate(qubit, theta):
+    if theta is not None:
+        gate = Rz(qubit, theta)
+        Rzgate = Gate(
+            sympy.Matrix(
+                [
+                    [sympy.exp(-1 * sympy.I * theta / 2), 0],
+                    [0, sympy.exp(sympy.I * theta / 2)],
+                ]
+            ),
+            (qubit,),
+        )
+    else:
+        gate = Rz(qubit)
+        Rzgate = Gate(
+            sympy.Matrix(
+                [
+                    [sympy.exp(-1 * sympy.I * sympy.Symbol("theta") / 2), 0],
+                    [0, sympy.exp(sympy.I * sympy.Symbol("theta") / 2)],
+                ]
+            ),
+            (qubit,),
+        )
+    assert gate == Rzgate

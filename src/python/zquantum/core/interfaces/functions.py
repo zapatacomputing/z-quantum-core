b748485a9e7410414058be7d9b383579c641d090
dexter2206 <dexter2206@gmail.com>
2020-10-02 19:34:04 +0200

    Implement function_with_gradient helper

--- src/python/zquantum/core/interfaces/functions.py
+++ src/python/zquantum/core/interfaces/functions.py
@@ -1,7 +1,8 @@
 """Protocols describing different kinds of functions."""
-from typing import NamedTuple, Callable, Any, TypeVar, Optional
+from typing import NamedTuple, Callable, Any, TypeVar, Optional, Union
 from typing_extensions import Protocol, runtime_checkable
 import numpy as np
+from zquantum.core.history.recorder import has_store_artifact_param
 
 T = TypeVar("T", covariant=True)
 S = TypeVar("S", contravariant=True)
@@ -50,3 +51,27 @@ class FunctionWithGradient(NamedTuple):
 
     def __call__(self, params: np.ndarray):
         return self.function(params)
+
+
+class FunctionWithGradientStoringArtifacts(NamedTuple):
+    """A callable with gradient that also stores artifacts."""
+    function: CallableStoringArtifacts
+    gradient: Callable[[np.ndarray], np.ndarray]
+
+    def __call__(self, params: np.ndarray, store_artifact: StoreArtifact):
+        return self.function(params, store_artifact)
+
+
+def function_with_gradient(
+    function: Union[Callable[[np.ndarray], float], CallableStoringArtifacts],
+    gradient: Callable[[np.ndarray], np.ndarray]
+):
+    """Combine function and gradient into an entity adhering to protocols used by history recorder.
+
+    Note that this is a preferred method for adding gradient to your function,
+    as it should automatically detect whether the function stores artifact or not.
+    """
+    if has_store_artifact_param(function):
+        return FunctionWithGradientStoringArtifacts(function, gradient)
+    else:
+        return FunctionWithGradient(function, gradient)

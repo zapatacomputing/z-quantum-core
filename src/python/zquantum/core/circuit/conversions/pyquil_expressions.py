0fd4569cf461cf46674651652d6e85a3e5a54f90
dexter2206 <dexter2206@gmail.com>
2020-11-12 14:56:20 +0100

    Restructure modules related to symbolic expressions manipulation

---
+++ src/python/zquantum/core/circuit/conversions/pyquil_expressions.py
@@ -0,0 +1,25 @@
+"""Utilities related to Quil based symbolic expressions."""
+import operator
+import pyquil
+from pyquil import quilatom
+from .symbolic_expressions import ExpressionDialect
+
+# Dialect defining conversion of intermediate expression tree to
+# the expression based on quil functions/parameters.
+# This is intended to be passed by a `dialect` argument of `translate_expression`.
+QUIL_DIALECT = ExpressionDialect(
+    symbol_factory=lambda symbol: pyquil.quil.Parameter(symbol.name),
+    number_factory=lambda number: number,
+    known_functions={
+        "add": operator.add,
+        "mul": operator.mul,
+        "div": operator.truediv,
+        "sub": operator.sub,
+        "pow": operator.pow,
+        "cos": quilatom.quil_cos,
+        "sin": quilatom.quil_sin,
+        "exp": quilatom.quil_exp,
+        "sqrt": quilatom.quil_sqrt,
+        "tan": lambda arg: quilatom.quil_sin(arg) / quilatom.quil_cos(arg)
+    }
+)

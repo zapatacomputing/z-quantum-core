50088b218499d47688c09283e15a929dea43cc2c
dexter2206 <dexter2206@gmail.com>
2020-12-17 15:21:30 +0100

    Switch from using loops to fixtures

--- src/python/zquantum/core/interfaces/optimizer_test.py
+++ src/python/zquantum/core/interfaces/optimizer_test.py
@@ -17,40 +17,39 @@ def sum_x_squared(x):
 
 
 class OptimizerTests(object):
-    # To run tests with this base class, the following variables need to be properly initialized in the child class:
-    # self.optimizers
+    # To run tests with this class, Inherit it, and in the same module
+    # define a fixture with name optimizer that returns an optimizer.
 
-    def test_optimizer_succeeds_with_optimizing_rosenbrock_function(self):
-        for optimizer in self.optimizers:
-            cost_function = FunctionWithGradient(rosenbrock_function, finite_differences_gradient(rosenbrock_function))
+    def test_optimizer_succeeds_with_optimizing_rosenbrock_function(self, optimizer):
+        cost_function = FunctionWithGradient(rosenbrock_function, finite_differences_gradient(rosenbrock_function))
 
-            results = optimizer.minimize(cost_function, initial_params=np.array([0, 0]))
-            self.assertAlmostEqual(results.opt_value, 0, places=4)
-            self.assertAlmostEqual(results.opt_params[0], 1, places=3)
-            self.assertAlmostEqual(results.opt_params[1], 1, places=3)
+        results = optimizer.minimize(cost_function, initial_params=np.array([0, 0]))
+        assert results.opt_value == pytest.approx(0, abs=1e-4)
+        assert results.opt_params == pytest.approx(np.ones(2), abs=1e-3)
+        assert results.opt_params[0] == pytest.approx(1, abs=1e-3)
+        assert results.opt_params[1] == pytest.approx(1, abs=1e-3)
 
-            self.assertIn("nfev", results.keys())
-            self.assertIn("nit", results.keys())
-            self.assertIn("opt_value", results.keys())
-            self.assertIn("opt_params", results.keys())
-            self.assertIn("history", results.keys())
+        assert "nfev" in results
+        assert "nit" in results
+        assert "opt_value" in results
+        assert "opt_params" in results
+        assert "history" in results
 
-    def test_optimizer_succeeds_with_optimizing_sum_of_squares_function(self):
-        for optimizer in self.optimizers:
-            cost_function = FunctionWithGradient(sum_x_squared, finite_differences_gradient(sum_x_squared))
+    def test_optimizer_succeeds_with_optimizing_sum_of_squares_function(self, optimizer):
+        cost_function = FunctionWithGradient(sum_x_squared, finite_differences_gradient(sum_x_squared))
 
-            results = optimizer.minimize(
-                cost_function, initial_params=np.array([1, -1])
-            )
-            self.assertAlmostEqual(results.opt_value, 0, places=5)
-            self.assertAlmostEqual(results.opt_params[0], 0, places=4)
-            self.assertAlmostEqual(results.opt_params[1], 0, places=4)
+        results = optimizer.minimize(
+            cost_function, initial_params=np.array([1, -1])
+        )
 
-            self.assertIn("nfev", results.keys())
-            self.assertIn("nit", results.keys())
-            self.assertIn("opt_value", results.keys())
-            self.assertIn("opt_params", results.keys())
-            self.assertIn("history", results.keys())
+        assert results.opt_value == pytest.approx(0, abs=1e-5)
+        assert results.opt_params == pytest.approx(np.zeros(2), abs=1e-4)
+
+        assert "nfev" in results
+        assert "nit" in results
+        assert "opt_value" in results
+        assert "opt_params" in results
+        assert "history" in results
 
 
 def test_optimization_result_contains_opt_value_and_opt_params():

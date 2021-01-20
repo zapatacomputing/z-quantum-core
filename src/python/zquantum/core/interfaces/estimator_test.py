18ff70262696b41b0c01c3de2b35714dc45b3d9b
michalstechly <michal.stechly@gmail.com>
2021-01-20 12:11:59 -0500

    Updated estimator tests to pytest syntax.

--- src/python/zquantum/core/interfaces/estimator_test.py
+++ src/python/zquantum/core/interfaces/estimator_test.py
@@ -1,28 +1,18 @@
-import unittest
+import pytest
 from ..measurement import ExpectationValues
 
 
-class EstimatorTests(object):
-    # To run tests with this base class, the following variables need to be properly initialized in the child class:
-    # self.estimators
-    # self.backend
-    # self.circuit
-    # self.operator
-    # self.n_samples
-    # self.epsilon
-    # self.delta
-
-    def test_get_estimated_expectation_values_returns_expectation_values(self):
-        for estimator in self.estimators:
-            # Given
-            # When
-            value = estimator.get_estimated_expectation_values(
-                self.backend,
-                self.circuit,
-                self.operator,
-                n_samples=self.n_samples,
-                epsilon=self.epsilon,
-                delta=self.delta,
-            )
-            # Then
-            self.assertIsInstance(value, ExpectationValues)
+class EstimatorTests:
+    def test_get_estimated_expectation_values_returns_expectation_values(
+        self, estimator, backend, circuit, operator, n_samples, epsilon, delta
+    ):
+        value = estimator.get_estimated_expectation_values(
+            backend,
+            circuit,
+            operator,
+            n_samples=n_samples,
+            epsilon=epsilon,
+            delta=delta,
+        )
+        # Then
+        assert type(value) is ExpectationValues

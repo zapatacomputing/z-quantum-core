f134b5f2e19267eb7ff10e4c37dfb5b5fd546f5e
Jerome Gonthier <jerome@zapatacomputing.com>
2021-01-27 21:19:08 -0500

    Adding step to evaluate a qubit operator list

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -2,13 +2,18 @@ from pyquil.wavefunction import Wavefunction
 import json
 import numpy as np
 from typing import Dict, Union, List
-from openfermion import SymbolicOperator
+from openfermion import SymbolicOperator, QubitOperator
 from openfermion.utils import (
     qubit_operator_sparse,
     jw_get_ground_state_at_particle_number as _jw_get_ground_state_at_particle_number,
 )
 
-from zquantum.core.measurement import save_expectation_values, save_wavefunction
+from zquantum.core.measurement import (
+    save_expectation_values,
+    save_wavefunction,
+    load_expectation_values,
+    ExpectationValues,
+)
 from zquantum.core.circuit import (
     load_circuit,
     load_parameter_grid,
@@ -20,8 +25,10 @@ from zquantum.core.circuit import (
 from zquantum.core.utils import create_object, ValueEstimate, save_value_estimate
 from zquantum.core.openfermion import (
     load_qubit_operator,
+    load_qubit_operator_set,
     evaluate_operator_for_parameter_grid as _evaluate_operator_for_parameter_grid,
     save_parameter_grid_evaluation,
+    evaluate_qubit_operator_list as _evaluate_qubit_operator_list,
 )
 
 
@@ -124,4 +131,20 @@ def jw_get_ground_state_at_particle_number(
     value_estimate = ValueEstimate(ground_energy)
 
     save_wavefunction(ground_state, "ground-state.json")
+    save_value_estimate(value_estimate, "value-estimate.json")
+
+
+def evaluate_qubit_operator_list(
+    qubit_operator_list: Union[str, List[QubitOperator]],
+    expectation_values: Union[str, ExpectationValues],
+):
+    if isinstance(qubit_operator_list, str):
+        qubit_operator_list = load_qubit_operator_set(qubit_operator_list)
+    if isinstance(expectation_values, str):
+        expectation_values = load_expectation_values(expectation_values)
+
+    value_estimate = _evaluate_qubit_operator_list(
+        qubit_operator_list, expectation_values
+    )
+
     save_value_estimate(value_estimate, "value-estimate.json")

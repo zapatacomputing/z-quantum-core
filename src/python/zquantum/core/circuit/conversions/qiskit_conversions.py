1466167d2adb85f50ffa40afc2c0a5d4c406d4d3
dexter2206 <dexter2206@gmail.com>
2021-02-02 11:32:07 +0100

    Add stubs for qiskit conversion functions

---
+++ src/python/zquantum/core/circuit/conversions/qiskit_conversions.py
@@ -0,0 +1,18 @@
+from typing import Tuple, List, Union
+
+import qiskit
+from zquantum.core.circuit import X, Y, Z, I, T, H, Gate, Circuit
+
+QiskitOperation = Tuple[
+    qiskit.circuit.Instruction, List[qiskit.circuit.Qubit], List[qiskit.circuit.Clbit]
+]
+
+
+def convert_from_qiskit(
+    obj: Union[QiskitOperation, qiskit.QuantumCircuit]
+) -> Union[Gate, Circuit]:
+    pass
+
+
+def convert_to_qiskit(obj):
+    pass

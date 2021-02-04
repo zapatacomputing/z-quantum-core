2ebf932ed94d72f7abfed41d87f5d9b940944310
dexter2206 <dexter2206@gmail.com>
2021-02-04 15:15:39 +0100

    Initial implementation of integer_pow

---
+++ src/python/zquantum/core/circuit/conversions/symbolic/qiskit_expressions.py
@@ -0,0 +1,37 @@
+import operator
+from functools import reduce
+
+from .expressions import ExpressionDialect, FunctionCall
+import qiskit
+
+from .helpers import reduction
+
+
+def integer_pow(pow_call: FunctionCall):
+    base, exponent = pow_call.args
+    if not isinstance(exponent, int):
+        raise ValueError(
+            f"Cannot convert expression {base} ** {exponent} to Qiskit. "
+            "Only powers with integral exponent are convertible."
+        )
+    if exponent < 0:
+        if base != 0:
+            base = 1 / base
+        else:
+            raise ValueError(
+                f"Invalid power: cannot raise 0 to exponent {exponent} < 0."
+            )
+    return reduce(operator.mul, exponent * [base], 1)
+
+
+QISKIT_DIALECT = ExpressionDialect(
+    symbol_factory=lambda symbol: qiskit.circuit.Parameter(symbol.name),
+    number_factory=lambda number: number,
+    known_functions={
+        "add": reduction(operator.add),
+        "mul": reduction(operator.mul),
+        "div": operator.truediv,
+        "sub": operator.sub,
+        "pow": operator.pow,
+    }
+)
---
+++ src/python/zquantum/core/circuit/conversions/symbolic/qiskit_expressions_test.py
@@ -0,0 +1,25 @@
+import pytest
+import qiskit
+
+from .expressions import FunctionCall
+from .qiskit_expressions import integer_pow
+
+
+THETA = qiskit.circuit.Parameter("theta")
+
+
+class TestIntegerPower:
+
+    @pytest.mark.parametrize(
+        "base, exponent, expected_result",
+        [
+            (2.5, 3, 2.5 ** 3),
+            (THETA, 2, THETA * THETA)
+        ]
+    )
+    def test_integer_power_with_positive_exponent_is_correctly_converted_to_number(
+        self, base, exponent, expected_result
+    ):
+        power = FunctionCall("pow", (base, exponent))
+        assert integer_pow(power) == expected_result
+

b0ffa6e519f909e6129d7b6ada4f77531dc9a974
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-02-09 17:31:49 +0100

    Run black

--- src/python/zquantum/core/circuit/conversions/symbolic/qiskit_expressions.py
+++ src/python/zquantum/core/circuit/conversions/symbolic/qiskit_expressions.py
@@ -11,7 +11,7 @@ from .sympy_expressions import expression_from_sympy
 
 @singledispatch
 def expression_from_qiskit(expression):
-    '''Parse Qiskit expression into intermediate expression tree.'''
+    """Parse Qiskit expression into intermediate expression tree."""
     raise NotImplementedError(
         f"Expression {expression} of type {type(expression)} is currently not supported"
     )
@@ -23,7 +23,9 @@ def _number_identity(number: Number):
 
 
 @expression_from_qiskit.register
-def _expr_from_qiskit_param_expr(qiskit_expr: qiskit.circuit.parameterexpression.ParameterExpression):
+def _expr_from_qiskit_param_expr(
+    qiskit_expr: qiskit.circuit.parameterexpression.ParameterExpression,
+):
     # At the moment of writing this (qiskit==0.16.1) there's no other way to introspect
     # a Qiskit parameter expression than by using a property from the private API.
     sympy_expr = qiskit_expr._symbol_expr
@@ -59,5 +61,5 @@ QISKIT_DIALECT = ExpressionDialect(
         "div": operator.truediv,
         "sub": operator.sub,
         "pow": integer_pow,
-    }
+    },
 )

4e76f6c81fc5de14abf566e8f0c796f3147fc320
Alexander Kunitsa <28026247+aakunitsa@users.noreply.github.com>
2021-02-18 17:58:46 -0500

    moved functions from utils to circuit in steps

--- steps/circuit.py
+++ steps/circuit.py
@@ -17,8 +17,26 @@ from zquantum.core.circuit import (
 )
 from zquantum.core.testing import create_random_circuit as _create_random_circuit
 from typing import Union, List, Optional
-from utils import Specs, load_from_specs
+#from .utils import Specs, load_from_specs
 
+from typing import Dict
+from zquantum.core.utils import save_list, create_object
+
+Specs = Union[str, Dict]
+
+def load_from_specs(specs):
+    if isinstance(specs, str):
+        specs = json.loads(specs)
+    return create_object(specs)
+
+# Generate random list of integers
+def generate_random_list_of_integers(seed: Union[None, int] = None, **kwargs):
+    """Generate a random list of integers. Docs can be found at the link below, but the keyword arugment "low" is mandatory.
+    https://numpy.org/devdocs/reference/random/generated/numpy.random.Generator.integers.html#numpy.random.Generator.integers
+    """
+    rng = np.random.default_rng(seed=seed)
+    generated_integers = rng.integers(**kwargs).tolist()
+    save_list(generated_integers, "integers.json")
 
 # Generate random parameters for an ansatz
 def generate_random_ansatz_params(

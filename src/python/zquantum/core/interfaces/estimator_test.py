66803cfab71e34a8e18e5ba885f3c658a0832684
mstechly <michal.stechly@gmail.com>
2021-02-18 11:18:24 -0500

    Minor refactor.

--- src/python/zquantum/core/interfaces/estimator_test.py
+++ src/python/zquantum/core/interfaces/estimator_test.py
@@ -4,14 +4,14 @@ from ..measurement import ExpectationValues
 
 class EstimatorTests:
     def test_get_estimated_expectation_values_returns_expectation_values(
-        self, estimator, backend, circuit, operator, n_samples, **kwargs
+        self, estimator, backend, circuit, operator, n_samples, **estimator_kwargs
     ):
         value = estimator.get_estimated_expectation_values(
             backend=backend,
             circuit=circuit,
             target_operator=operator,
             n_samples=n_samples,
-            **kwargs
+            **estimator_kwargs
         )
         # Then
         assert type(value) is ExpectationValues

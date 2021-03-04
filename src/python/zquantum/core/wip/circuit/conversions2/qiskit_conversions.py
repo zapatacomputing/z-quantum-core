07eeb825bcc257bddae2cee1c73c6f11cdc4e936
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-03-04 15:00:34 +0100

    Implement converting basic orquestra circuits to qiskit

--- src/python/zquantum/core/wip/circuit/conversions2/qiskit_conversions.py
+++ src/python/zquantum/core/wip/circuit/conversions2/qiskit_conversions.py
@@ -1,1 +1,72 @@
-# TODO: implement zquantum<->qiskit circuit conversions here
+import qiskit
+from typing import Tuple, List
+
+from .. import _gates as g
+from ..symbolic.sympy_expressions import expression_from_sympy, SYMPY_DIALECT
+from ..symbolic.qiskit_expressions import expression_from_qiskit, QISKIT_DIALECT
+from ..symbolic.translations import translate_expression
+
+QiskitOperation = Tuple[
+    qiskit.circuit.Instruction, List[qiskit.circuit.Qubit], List[qiskit.circuit.Clbit]
+]
+
+
+def qiskit_qubit(index: int, num_qubits_in_circuit: int) -> qiskit.circuit.Qubit:
+    return qiskit.circuit.Qubit(
+        qiskit.circuit.QuantumRegister(num_qubits_in_circuit, "q"), index
+    )
+
+
+def _qiskit_expr_from_zquantum(expr):
+    intermediate = expression_from_sympy(expr)
+    return translate_expression(intermediate, QISKIT_DIALECT)
+
+
+ZQUANTUM_QISKIT_GATE_MAP = {
+    "X": qiskit.circuit.library.XGate,
+    "Y": qiskit.circuit.library.YGate,
+    "Z": qiskit.circuit.library.ZGate,
+    "T": qiskit.circuit.library.TGate,
+    "H": qiskit.circuit.library.HGate,
+    "I": qiskit.circuit.library.IGate,
+    "CNOT": qiskit.circuit.library.CXGate,
+    "CZ": qiskit.circuit.library.CZGate,
+    "SWAP": qiskit.circuit.library.SwapGate,
+    "ISWAP": qiskit.circuit.library.iSwapGate,
+    "RX": qiskit.circuit.library.RXGate,
+    "RY": qiskit.circuit.library.RYGate,
+    "RZ": qiskit.circuit.library.RZGate,
+    "PHASE": qiskit.circuit.library.PhaseGate,
+    "CPHASE": qiskit.circuit.library.CPhaseGate,
+    "XX": qiskit.circuit.library.RXXGate,
+    "YY": qiskit.circuit.library.RYYGate,
+    "ZZ": qiskit.circuit.library.RZZGate,
+}
+
+
+def _convert_gate_op_to_qiskit(gate_op: g.GateOperation, n_qubits_in_circuit) -> QiskitOperation:
+    qiskit_params = [_qiskit_expr_from_zquantum(param) for param in gate_op.gate.params]
+    qiskit_qubits = [
+        qiskit_qubit(qubit_i, n_qubits_in_circuit) for qubit_i in gate_op.qubit_indices
+    ]
+    try:
+        qiskit_cls = ZQUANTUM_QISKIT_GATE_MAP[gate_op.gate.name]
+        return qiskit_cls(*qiskit_params), qiskit_qubits, []
+    except KeyError:
+        raise NotImplementedError(f"Conversion of {gate_op.gate} to Qiskit is unsupported.")
+
+
+def convert_to_qiskit(circuit: g.Circuit) -> qiskit.QuantumCircuit:
+    if circuit.free_symbols:
+        raise NotImplementedError(
+            "Converting parametrized circuits to Qiskit is unsupported"
+        )
+
+    q_circuit = qiskit.QuantumCircuit(circuit.n_qubits)
+    q_triplets = [
+        _convert_gate_op_to_qiskit(gate_op, circuit.n_qubits)
+        for gate_op in circuit.operations
+    ]
+    for q_gate, q_qubits, q_clbits in q_triplets:
+        q_circuit.append(q_gate, q_qubits, q_clbits)
+    return q_circuit
---
+++ src/python/zquantum/core/wip/circuit/conversions2/qiskit_conversions_test.py
@@ -0,0 +1,126 @@
+import sympy
+import numpy as np
+import qiskit
+import pytest
+
+from .qiskit_conversions import convert_to_qiskit
+from .. import _gates as g
+from .. import _builtin_gates as bg
+
+
+# NOTE: In Qiskit, 0 is the most significant qubit,
+# whereas in ZQuantum, 0 is the least significant qubit.
+# This is we need to flip the indices.
+#
+# See more at
+# https://qiskit.org/documentation/tutorials/circuits/1_getting_started_with_qiskit.html#Visualize-Circuit
+
+
+def _single_qubit_qiskit_circuit():
+    qc = qiskit.QuantumCircuit(6)
+    qc.x(0)
+    qc.z(2)
+    return qc
+
+
+def _two_qubit_qiskit_circuit():
+    qc = qiskit.QuantumCircuit(4)
+    qc.cnot(0, 1)
+    return qc
+
+
+def _parametric_qiskit_circuit(angle):
+    qc = qiskit.QuantumCircuit(4)
+    qc.rx(angle, 1)
+    return qc
+
+
+def _qiskit_circuit_with_controlled_gate():
+    qc = qiskit.QuantumCircuit(5)
+    qc.append(qiskit.extensions.SwapGate().control(1), [2, 0, 3])
+    return qc
+
+
+SYMPY_THETA = sympy.Symbol("theta")
+SYMPY_GAMMA = sympy.Symbol("gamma")
+QISKIT_THETA = qiskit.circuit.Parameter("theta")
+QISKIT_GAMMA = qiskit.circuit.Parameter("gamma")
+
+
+EQUIVALENT_CIRCUITS = [
+    (
+        g.Circuit(
+            [
+                bg.X(0),
+                bg.Z(2),
+            ],
+            6,
+        ),
+        _single_qubit_qiskit_circuit(),
+    ),
+    (
+        g.Circuit(
+            [
+                bg.CNOT(0, 1),
+            ],
+            4,
+        ),
+        _two_qubit_qiskit_circuit(),
+    ),
+    (
+        g.Circuit(
+            [
+                bg.RX(np.pi)(1),
+            ],
+            4,
+        ),
+        _parametric_qiskit_circuit(np.pi),
+    ),
+    # (
+    #     g.Circuit(
+    #         [
+    #             bg.SWAP.controlled(1)(2, 0, 3)
+    #         ],
+    #         5,
+    #     ),
+    #     _qiskit_circuit_with_controlled_gate(),
+    # ),
+]
+
+
+EQUIVALENT_PARAMETRIZED_CIRCUITS = [
+    (
+        g.Circuit(
+            [
+                bg.RX(SYMPY_THETA)(1),
+            ],
+            4,
+        ),
+        _parametric_qiskit_circuit(QISKIT_THETA),
+    ),
+]
+
+
+def _draw_qiskit_circuit(circuit):
+    return qiskit.visualization.circuit_drawer(circuit, output="text")
+
+
+class TestQiskitCircuitConversion:
+    @pytest.mark.parametrize("zquantum_circuit, qiskit_circuit", EQUIVALENT_CIRCUITS)
+    def test_converting_circuit_gives_equivalent_circuit(
+        self, zquantum_circuit, qiskit_circuit
+    ):
+        converted = convert_to_qiskit(zquantum_circuit)
+        assert converted == qiskit_circuit, (
+            f"Converted circuit:\n{_draw_qiskit_circuit(converted)}\n isn't equal "
+            f"to\n{_draw_qiskit_circuit(qiskit_circuit)}"
+        )
+
+    @pytest.mark.parametrize("zquantum_circuit, qiskit_circuit", EQUIVALENT_PARAMETRIZED_CIRCUITS)
+    def test_converting_parametrized_circuit_(
+        self, zquantum_circuit, qiskit_circuit
+    ):
+        # NOTE: parametrized circuit conversion is unsupported broken because qiskit
+        # requires using singleton parameter objects
+        with pytest.raises(NotImplementedError):
+            convert_to_qiskit(zquantum_circuit)

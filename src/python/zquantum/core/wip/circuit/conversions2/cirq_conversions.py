ac8400ef0a7e5f65ed73ebd4c1beca7ce84aae66
Konrad Jałowiecki <dexter2206@gmail.com>
2021-03-08 10:27:27 +0100

    Add conversion of Pauli gates

--- src/python/zquantum/core/wip/circuit/conversions2/cirq_conversions.py
+++ src/python/zquantum/core/wip/circuit/conversions2/cirq_conversions.py
@@ -1,1 +1,62 @@
-# TODO: implement zquantum<->cirq circuit conversions here
+"""Orquestra <-> Cirq conversions."""
+from functools import singledispatch
+import cirq
+
+from .. import _builtin_gates as bg
+from .. import _gates as g
+
+
+ORQUESTRA_BUILTIN_GATE_NAME_TO_CIRQ_GATE = {
+    "X": cirq.X,
+    "Y": cirq.Y,
+    "Z": cirq.Z
+}
+
+
+EIGENGATE_SPECIAL_CASES = {
+    (type(cirq.X), cirq.X.global_shift, cirq.X.exponent): bg.X,
+    (type(cirq.Y), cirq.Y.global_shift, cirq.Y.exponent): bg.Y,
+    (type(cirq.Z), cirq.Z.global_shift, cirq.Z.exponent): bg.Z,
+}
+
+
+@singledispatch
+def convert_to_cirq(obj):
+    raise NotImplementedError(f"{obj} not convertible to Cirq object.")
+
+
+@convert_to_cirq.register
+def convert_matrix_factory_gate_to_cirq(gate: g.MatrixFactoryGate) -> cirq.Gate:
+    try:
+        return ORQUESTRA_BUILTIN_GATE_NAME_TO_CIRQ_GATE[gate.name]
+    except KeyError:
+        raise NotImplementedError(f"Gate {gate} not convertible to Cirq.")
+
+
+@convert_to_cirq.register
+def convert_gate_operation_to_cirq(operation: g.GateOperation) -> cirq.GateOperation:
+    return convert_to_cirq(operation.gate)(*map(cirq.LineQubit, operation.qubit_indices))
+
+
+@singledispatch
+def convert_from_cirq(obj):
+    raise NotImplementedError(f"{obj} not convertible to Orquestra object.")
+
+
+@convert_from_cirq.register
+def convert_eigengate_to_orquestra_gate(eigengate: cirq.EigenGate) -> g.Gate:
+    key = (type(eigengate), eigengate.global_shift, eigengate.exponent)
+    if key in EIGENGATE_SPECIAL_CASES:
+        return EIGENGATE_SPECIAL_CASES[key]
+    else:
+        raise NotImplementedError(f"Gate {eigengate} not convertible to Orquestra object.")
+
+
+@convert_from_cirq.register
+def convert_gate_operation_to_orquestra(operation: cirq.GateOperation) -> g.GateOperation:
+    if not all(isinstance(qubit, cirq.LineQubit) for qubit in operation.qubits):
+        raise NotImplementedError(
+            f"Failed to convert {operation}. Grid qubits are not yet supported."
+        )
+
+    return convert_from_cirq(operation.gate)(*(qubit.x for qubit in operation.qubits))
---
+++ src/python/zquantum/core/wip/circuit/conversions2/cirq_conversions_test.py
@@ -0,0 +1,31 @@
+"""Test cases from Orquestra <-> cirq conversions."""
+import cirq
+import pytest
+
+from .cirq_conversions import convert_from_cirq, convert_to_cirq
+from .. import _builtin_gates as bg
+
+
+@pytest.mark.parametrize(
+    "orquestra_operation, cirq_operation",
+    [
+        (bg.X(1), cirq.X(cirq.LineQubit(1))),
+        (bg.Y(2), cirq.Y(cirq.LineQubit(2))),
+        (bg.Z(0), cirq.Z(cirq.LineQubit(0)))
+    ]
+)
+class TestBuiltinGateOperationConversion:
+
+    def test_orquestra_builtin_gate_operation_is_converted_to_its_cirq_counterpart(
+        self,
+        orquestra_operation,
+        cirq_operation
+    ):
+        assert convert_to_cirq(orquestra_operation) == cirq_operation
+
+    def test_cirq_gate_operation_is_converted_to_its_orquestra_counterpart(
+        self,
+        cirq_operation,
+        orquestra_operation
+    ):
+        assert convert_from_cirq(cirq_operation) == orquestra_operation

fb823eaa0a94316a530a91b11ea1faa530d08262
Konrad Jałowiecki <dexter2206@gmail.com>
2021-03-09 14:59:28 +0100

    Use previously implemented tests instead of new ones

--- src/python/zquantum/core/wip/circuit/conversions2/cirq_conversions.py
+++ src/python/zquantum/core/wip/circuit/conversions2/cirq_conversions.py
@@ -1,10 +1,60 @@
 """Orquestra <-> Cirq conversions."""
 from functools import singledispatch
 from operator import attrgetter
+from typing import Union, Callable, Type
+
 import cirq
+import numpy as np
+import sympy
 
 from .. import _builtin_gates as bg
 from .. import _gates as g
+
+
+Parameter = Union[sympy.Expr, float]
+RotationGateFactory = Callable[[Parameter], cirq.EigenGate]
+
+
+def angle_to_exponent(angle: Parameter) -> Parameter:
+    """Convert exponent from Cirq gate to angle usable in rotation gates..
+
+    Args:
+        angle: Exponent to be converted.
+    Returns:
+        angle divided by pi.
+    Notes:
+        Scaling of the angle preserves its "type", i.e. numerical angles
+        are scaled by numerical approximation of pi, but symbolic ones
+        are scaled by `sympy.pi`.
+
+        This transformation might be viewed as the change of units from
+        radians to pi * radians.
+    """
+    return angle / (sympy.pi if isinstance(angle, sympy.Expr) else np.pi)
+
+
+def make_rotation_factory(
+    eigengate_cls: Type[cirq.EigenGate], global_shift: float = 0
+) -> RotationGateFactory:
+    """Construct a factory for rotation gate based on given EigenGate subclass.
+
+    This function might be thought of as a partial which freezes global_shift
+    parameter but also scales the exponent parameter of eigengate_cls initializer.
+
+    Args:
+        eigengate_cls: EigenGate subclass, e.g. ZPowGate, XXPowGate.
+        global_shift: Determines phase of the rotation gate. Check Cirq docs
+            for explanation.
+    Returns:
+        A function that maps angle to EigenGate instance with given global shift
+        and an exponent equal to angle divided by a factor of pi.
+    """
+    def _rotation(angle: Parameter) -> cirq.EigenGate:
+        return eigengate_cls(
+            global_shift=global_shift, exponent=angle_to_exponent(angle)
+        )
+
+    return _rotation
 
 
 ORQUESTRA_BUILTIN_GATE_NAME_TO_CIRQ_GATE = {
@@ -30,6 +80,19 @@ EIGENGATE_SPECIAL_CASES = {
     (type(cirq.CZ), cirq.CZ.global_shift, cirq.CZ.exponent): bg.CZ,
     (type(cirq.SWAP), cirq.SWAP.global_shift, cirq.SWAP.exponent): bg.SWAP,
 }
+
+EIGENGATE_ROTATIONS = {
+    (cirq.XPowGate, -0.5): bg.RX,
+    (cirq.YPowGate, -0.5): bg.RY,
+    (cirq.ZPowGate, -0.5): bg.RZ,
+    (cirq.ZPowGate, 0): bg.PHASE,
+    (cirq.CZPowGate, 0): bg.CPHASE,
+    (cirq.XXPowGate, -0.5): bg.XX,
+    (cirq.YYPowGate, -0.5): bg.YY,
+    (cirq.ZZPowGate, -0.5): bg.ZZ,
+    (cirq.ISwapPowGate, 0.0): bg.XY,
+}
+
 
 qubit_index = attrgetter("x")
 
--- src/python/zquantum/core/wip/circuit/conversions2/cirq_conversions_test.py
+++ src/python/zquantum/core/wip/circuit/conversions2/cirq_conversions_test.py
@@ -1,37 +1,138 @@
 """Test cases from Orquestra <-> cirq conversions."""
 import cirq
+import numpy as np
 import pytest
+import sympy
 
-from .cirq_conversions import convert_from_cirq, convert_to_cirq
+from .cirq_conversions import convert_from_cirq, convert_to_cirq, make_rotation_factory
 from .. import _builtin_gates as bg
 
 
-@pytest.mark.parametrize(
-    "orquestra_operation, cirq_operation",
+THETA = sympy.Symbol("theta")
+
+EXAMPLE_SYMBOLIC_ANGLES = [
+    sympy.Symbol("theta"),
+    sympy.Symbol("x") + sympy.Symbol("y"),
+    sympy.cos(sympy.Symbol("phi") / 2),
+]
+
+
+EQUIVALENT_NONPARAMETRIC_SINGLE_QUBIT_GATES = [
+    (bg.X, cirq.X),
+    (bg.Y, cirq.Y),
+    (bg.Z, cirq.Z),
+    (bg.T, cirq.T),
+    (bg.I, cirq.I),
+    (bg.H, cirq.H),
+]
+
+
+EQUIVALENT_SINGLE_QUBIT_ROTATION_GATES = [
+    (bg.RX, cirq.rx),
+    (bg.RY, cirq.ry),
+    (bg.RZ, cirq.rz),
+    (bg.PHASE, make_rotation_factory(cirq.ZPowGate)),
+]
+
+
+EQUIVALENT_NONPARAMETRIC_TWO_QUBIT_GATES = [
+    (bg.CZ, cirq.CZ),
+    (bg.CNOT, cirq.CNOT),
+    (bg.SWAP, cirq.SWAP),
+]
+
+
+TWO_QUBIT_ROTATION_GATE_FACTORIES = [
+    (bg.CPHASE, make_rotation_factory(cirq.CZPowGate)),
+    (bg.XX, make_rotation_factory(cirq.XXPowGate, global_shift=-0.5)),
+    (bg.YY, make_rotation_factory(cirq.YYPowGate, global_shift=-0.5)),
+    (bg.ZZ, make_rotation_factory(cirq.ZZPowGate, global_shift=-0.5)),
+    (bg.XY, make_rotation_factory(cirq.ISwapPowGate, 0.0))
+]
+
+
+# Here we combine multiple testcases of the form
+# (ZQuantum gate, Cirq operation)
+# We do this for easier parametrization in tests that follow.
+TEST_CASES_WITHOUT_SYMBOLIC_PARAMS = (
     [
-        (bg.X(1), cirq.X(cirq.LineQubit(1))),
-        (bg.Y(2), cirq.Y(cirq.LineQubit(2))),
-        (bg.Z(0), cirq.Z(cirq.LineQubit(0))),
-        (bg.I(5), cirq.I(cirq.LineQubit(5))),
-        (bg.H(0), cirq.H(cirq.LineQubit(0))),
-        (bg.T(1), cirq.T(cirq.LineQubit(1))),
-        (bg.CNOT(1, 2), cirq.CNOT(cirq.LineQubit(1), cirq.LineQubit(2))),
-        (bg.CZ(1, 0), cirq.CZ(cirq.LineQubit(1), cirq.LineQubit(0))),
-        (bg.SWAP(2, 0), cirq.SWAP(cirq.LineQubit(2), cirq.LineQubit(0)))
+        (orq_gate(q), cirq_gate.on(cirq.LineQubit(q)))
+        for orq_gate, cirq_gate in EQUIVALENT_NONPARAMETRIC_SINGLE_QUBIT_GATES
+        for q in [0, 1, 5, 13]
+    ]
+    + [
+        (orq_gate(q0, q1), cirq_gate.on(cirq.LineQubit(q0), cirq.LineQubit(q1)))
+        for orq_gate, cirq_gate in EQUIVALENT_NONPARAMETRIC_TWO_QUBIT_GATES
+        for q0, q1 in [(0, 1), (2, 3), (0, 10)]
+    ]
+    + [
+        (orq_gate_factory(angle)(q), cirq_gate_func(angle).on(cirq.LineQubit(q)))
+        for orq_gate_factory, cirq_gate_func in EQUIVALENT_SINGLE_QUBIT_ROTATION_GATES
+        for q in [0, 4, 10, 11]
+        for angle in [0, np.pi, np.pi / 2, 0.4]
+    ]
+    + [
+        (
+            orq_gate_cls(q0, q1, angle),
+            cirq_gate_func(angle).on(cirq.LineQubit(q0), cirq.LineQubit(q1)),
+        )
+        for orq_gate_cls, cirq_gate_func in TWO_QUBIT_ROTATION_GATE_FACTORIES
+        for q0, q1 in [(0, 1), (2, 3), (0, 10)]
+        for angle in [np.pi, np.pi / 2, np.pi / 5, 0.4, 0.1, 0.05, 2.5]
     ]
 )
-class TestBuiltinGateOperationConversion:
 
-    def test_orquestra_builtin_gate_operation_is_converted_to_its_cirq_counterpart(
-        self,
-        orquestra_operation,
-        cirq_operation
+
+TEST_CASES_WITH_SYMBOLIC_PARAMS = [
+    (orq_gate_factory(angle)(q), cirq_gate_func(angle).on(cirq.LineQubit(q)))
+    for orq_gate_factory, cirq_gate_func in EQUIVALENT_SINGLE_QUBIT_ROTATION_GATES
+    for q in [0, 4, 10, 11]
+    for angle in EXAMPLE_SYMBOLIC_ANGLES
+] + [
+    (
+        orq_gate_factory(angle)(q0, q1),
+        cirq_gate_func(angle).on(cirq.LineQubit(q0), cirq.LineQubit(q1)),
+    )
+    for orq_gate_factory, cirq_gate_func in TWO_QUBIT_ROTATION_GATE_FACTORIES
+    for q0, q1 in [(0, 1), (2, 3), (0, 10)]
+    for angle in EXAMPLE_SYMBOLIC_ANGLES
+]
+
+
+@pytest.mark.parametrize(
+    "orquestra_operation, cirq_operation", TEST_CASES_WITHOUT_SYMBOLIC_PARAMS
+)
+class TestGateConversionWithoutSymbolicParameters:
+    def test_converting_orquestra_gate_operation_to_cirq_gives_expected_operation(
+        self, orquestra_operation, cirq_operation
     ):
         assert convert_to_cirq(orquestra_operation) == cirq_operation
 
-    def test_cirq_gate_operation_is_converted_to_its_orquestra_counterpart(
-        self,
-        cirq_operation,
-        orquestra_operation
+    def test_converting_cirq_operation_to_orquestra_gives_expected_gate_operation(
+        self, orquestra_operation, cirq_operation
+    ):
+        assert convert_from_cirq(cirq_operation) == orquestra_operation
+
+    def test_orquestra_gate_and_cirq_gate_have_the_same_matrix(
+        self, orquestra_operation, cirq_operation
+    ):
+        # This is to ensure that we are indeed converting the same gate.
+        assert np.allclose(
+            np.array(orquestra_operation.gate.matrix).astype(np.complex128),
+            cirq.unitary(cirq_operation.gate),
+        )
+
+
+@pytest.mark.parametrize(
+    "orquestra_operation, cirq_operation", TEST_CASES_WITH_SYMBOLIC_PARAMS
+)
+class TestGateConversionWithSymbolicParameters:
+    def test_converting_orquestra_gate_operation_to_cirq_gives_expected_operation(
+        self, orquestra_operation, cirq_operation
+    ):
+        assert convert_to_cirq(orquestra_operation) == cirq_operation
+
+    def test_converting_cirq_operation_to_orquestra_gives_expected_gate_operation(
+        self, orquestra_operation, cirq_operation
     ):
         assert convert_from_cirq(cirq_operation) == orquestra_operation

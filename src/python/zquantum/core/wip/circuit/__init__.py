731b965939cc367a5e63599749cf72c83f374f21
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-03-16 13:22:00 +0100

    Define new circuits' public API with a package init

--- src/python/zquantum/core/wip/circuit/__init__.py
+++ src/python/zquantum/core/wip/circuit/__init__.py
@@ -1,1 +1,53 @@
+"""Public API for the ZQuantum circuits."""
 
+from ._gates import (
+    Gate,
+    GateOperation,
+    MatrixFactoryGate,
+    ControlledGate,
+    Dagger,
+    CustomGateDefinition,
+    Circuit,
+)
+
+from ._builtin_gates import (
+    X,
+    Y,
+    Z,
+    H,
+    I,
+    T,
+    RX,
+    RY,
+    RZ,
+    PHASE,
+    CNOT,
+    CZ,
+    SWAP,
+    ISWAP,
+    CPHASE,
+    XX,
+    YY,
+    ZZ,
+    XY,
+)
+
+from ._serde import (
+    to_dict,
+    circuit_from_dict,
+)
+
+from .conversions2.cirq_conversions import (
+    export_to_cirq,
+    import_circuit_from_cirq,
+)
+
+from .conversions2.pyquil_conversions import (
+    export_to_pyquil,
+    import_from_pyquil,
+)
+
+from .conversions2.qiskit_conversions import (
+    export_to_qiskit,
+    import_from_qiskit,
+)

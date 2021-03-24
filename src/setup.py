cd2e83efbc4c55aa49837057c82f1e3ffec973a4
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-03-24 13:11:25 +0100

    Show readme on releases page

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,13 @@
+import os
+
 import setuptools
+
+
+def _read_readme():
+    this_path = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(this_path, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
 
 setuptools.setup(
     name="zquantum-core",
@@ -7,6 +16,8 @@ setuptools.setup(
     author="Zapata Computing, Inc.",
     author_email="info@zapatacomputing.com",
     description="A core library of the scientific code for Orquestra.",
+    long_description=_read_readme(),
+    long_description_content_type="text/markdown",
     url="https://github.com/zapatacomputing/z-quantum-core",
     packages=setuptools.find_namespace_packages(include=["zquantum.*"]),
     package_dir={"": "python"},

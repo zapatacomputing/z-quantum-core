66f47f2c580d7199bf3078d2d24f73d159be8805
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-03-26 11:39:45 +0100

    Move pytest to dev extras

--- src/setup.py
+++ src/setup.py
@@ -15,7 +15,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ),
     install_requires=[
-        "pytest>=5.3.5",
         "networkx==2.4",
         "numpy>=1.18.1",
         "scipy>=1.4.1",
@@ -28,4 +27,7 @@ setuptools.setup(
         "qiskit~=0.24",
         "overrides>=3.1.0",
     ],
+    extras_require={
+        "dev": ["pytest>=5.3.5"],
+    },
 )

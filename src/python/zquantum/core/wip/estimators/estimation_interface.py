7827ef209772f5c0154eb7e6cc73f9ee34a5615f
Michał Stęchły <michal.stechly@gmail.com>
2021-04-13 10:47:40 -0400

    Draft of new Estimator interface. (#259)
    * Draft of new Estimator interface.
    
    * Added missing docstrings & refactored in new estimator interface.
    
    * Changed file structure.
    
    * Added tests for new estimation code.
    
    * fixup! Added tests for new estimation code.
    
    * Updated docstrings.
    
    * Made EstimationTask a frozen dataclass.
    
    * Fixed typing in wip estimation tests.
    
    * Removed unnecessary typing.

---
+++ src/python/zquantum/core/wip/estimators/estimation_interface.py
@@ -0,0 +1,44 @@
+from ...circuit import Circuit
+from ...interfaces.backend import QuantumBackend
+from ...measurement import ExpectationValues
+from openfermion import SymbolicOperator
+from typing import List, Callable
+from dataclasses import dataclass
+from typing_extensions import Protocol
+
+
+@dataclass(frozen=True)
+class EstimationTask:
+    """
+    Data class defining an estimation problem.
+
+    Args:
+        operator: Operator for which we want to calculate the expectation values
+        circuit: Circuit used for evaluating the operator
+        constraints: Define constraints used in the estimation process,
+            e.g. number of shots or target accuracy.
+    """
+
+    operator: SymbolicOperator
+    circuit: Circuit
+    number_of_shots: int
+
+
+class EstimationTaskTransformer(Protocol):
+    """Protocol defining function which transforms a list of EstimationTasks
+    into another list of EstimationTasks.
+    """
+
+    def __call__(
+        self, estimation_problems: List[EstimationTask], **kwargs
+    ) -> List[EstimationTask]:
+        pass
+
+
+class EstimateExpectationValues(Protocol):
+    """Protocol defining function estimates expectation values for a list of estimation tasks.."""
+
+    def __call__(
+        self, backend: QuantumBackend, estimation_problems: List[EstimationTask]
+    ) -> ExpectationValues:
+        pass

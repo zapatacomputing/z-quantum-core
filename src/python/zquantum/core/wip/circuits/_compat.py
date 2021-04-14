16b453604716619a614f9075f33bee2d6eb17535
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-04-14 15:05:10 +0200

    Add support for compatibility for parametrized gates

--- src/python/zquantum/core/wip/circuits/_compat.py
+++ src/python/zquantum/core/wip/circuits/_compat.py
@@ -1,9 +1,11 @@
 import zquantum.core.circuit as old_circuit
 
 from . import Circuit
-from . import import_from_pyquil
+from . import import_from_pyquil, import_from_cirq
 
 
 def new_circuit_from_old_circuit(old: old_circuit.Circuit) -> Circuit:
-    return import_from_pyquil(old.to_pyquil())
-
+    try:
+        return import_from_pyquil(old.to_pyquil())
+    except NotImplementedError:
+        return import_from_cirq(old.to_cirq())
--- tests/zquantum/core/wip/circuits/_compat_test.py
+++ tests/zquantum/core/wip/circuits/_compat_test.py
@@ -1,5 +1,7 @@
 import pytest
 import pyquil
+import sympy
+import numpy as np
 import pyquil.gates
 
 import zquantum.core.circuit as old_circuit
@@ -8,8 +10,10 @@ import zquantum.core.wip.circuits as new_circuits
 
 
 PYQUIL_PROGRAMS = [
-    pyquil.Program([]),
-    pyquil.Program([pyquil.gates.X(2), pyquil.gates.Y(0)]),
+    pyquil.Program(),
+    pyquil.Program(pyquil.gates.X(2), pyquil.gates.Y(0)),
+    pyquil.Program(pyquil.gates.CNOT(3, 1)),
+    pyquil.Program(pyquil.gates.RX(np.pi, 1)),
 ]
 
 
@@ -21,11 +25,34 @@ def _new_circuit_from_pyquil(program):
     return new_circuits.import_from_pyquil(program)
 
 
+THETA_1 = sympy.Symbol("theta_1")
+
+def _make_old_parametric_circuit():
+    qubits = [old_circuit.Qubit(i) for i in range(0, 3)]
+    # theta_1 = sympy.Symbol("theta_1")
+    # theta_2 = sympy.Symbol("theta_2")
+
+    gate_RX_1 = old_circuit.Gate("Rx", params=[THETA_1], qubits=[qubits[0]])
+    # gate_RX_2 = old_circuit.Gate("Rx", params=[theta_2], qubits=[qubits[0]])
+
+    circ1 = old_circuit.Circuit()
+    circ1.qubits = qubits
+    circ1.gates = [gate_RX_1]
+
+    return circ1
+
+
 @pytest.mark.parametrize(
     "old,new",
     [
-        (_old_circuit_from_pyquil(program), _new_circuit_from_pyquil(program))
-        for program in PYQUIL_PROGRAMS
+        *[
+            (_old_circuit_from_pyquil(program), _new_circuit_from_pyquil(program))
+            for program in PYQUIL_PROGRAMS
+        ],
+        (
+            _make_old_parametric_circuit(),
+            new_circuits.Circuit([new_circuits.RX(THETA_1)(0)]), 
+        ),
     ],
 )
 def test_translated_circuit_matches_expected_circuit(old, new):

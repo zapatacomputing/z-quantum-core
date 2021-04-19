99563623c13523ae9c5832338d3fa87e6518c807
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-04-19 15:06:53 +0200

    Validate method overriding in runtime

--- src/python/zquantum/core/wip/interfaces/ansatz.py
+++ src/python/zquantum/core/wip/interfaces/ansatz.py
@@ -1,6 +1,8 @@
-from typing import Optional, Protocol
+from typing import Optional
+from abc import abstractmethod
 
 import numpy as np
+from typing_extensions import Protocol
 from zquantum.core.wip import circuits
 
 
@@ -8,31 +10,37 @@ class Ansatz(Protocol):
     """Interface for implementing ansatzes."""
 
     @property
+    @abstractmethod
     def number_of_layers(self) -> int:
         """Number of layers in the ansatz circuit."""
         # TODO: verify that docstring makes sense
         raise NotImplementedError()
 
     @property
+    @abstractmethod
     def supports_parametrized_circuits(self) -> bool:
         """If False, calling `parametrized_circuit` should raise an exception."""
         raise NotImplementedError()
 
     @property
+    @abstractmethod
     def number_of_qubits(self) -> int:
         """Number of qubits in the ansatz circuit."""
         raise NotImplementedError()
 
     @property
+    @abstractmethod
     def number_of_params(self) -> int:
         """Number of symbols in the ansatz circuit."""
         raise NotImplementedError()
 
     @property
+    @abstractmethod
     def parametrized_circuit(self) -> circuits.Circuit:
         """Parametrized circuit instance for this ansatz."""
         raise NotImplementedError()
 
+    @abstractmethod
     def generate_circuit(self, params: Optional[np.ndarray] = None) -> circuits.Circuit:
         """Circuit instance for this ansatz.
 
@@ -48,6 +56,7 @@ class Ansatz(Protocol):
         """
         raise NotImplementedError()
 
+    @abstractmethod
     def get_executable_circuit(self, params: np.ndarray) -> circuits.Circuit:
         """Circuit that can be directly executed without additional symbol substitution.
 

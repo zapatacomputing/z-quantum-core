59fd78452a6741b5ed3155aa3f89ede3a7a61e59
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-04-21 17:26:54 +0200

    Fix type hints for new circuits compatibility module

--- src/python/zquantum/core/wip/circuits/_compatibility.py
+++ src/python/zquantum/core/wip/circuits/_compatibility.py
@@ -1,8 +1,8 @@
-from ..circuits import Circuit as OldCircuit
+from ...circuit import Circuit as OldCircuit
 
-from . import Circuit
+from ._circuit import Circuit as NewCircuit
 from .conversions.cirq_conversions import import_from_cirq
 
 
-def new_circuit_from_old_circuit(old: OldCircuit) -> Circuit:
+def new_circuit_from_old_circuit(old: OldCircuit) -> NewCircuit:
     return import_from_cirq(old.to_cirq())

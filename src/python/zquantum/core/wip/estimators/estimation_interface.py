ccc0b2257cd4727249f132df6b391f39d9509ab5
mstechly <michal.stechly@gmail.com>
2021-04-28 14:12:46 -0400

    Update EstimateExpectationValues's docstring.

--- src/python/zquantum/core/wip/estimators/estimation_interface.py
+++ src/python/zquantum/core/wip/estimators/estimation_interface.py
@@ -36,7 +36,14 @@ class EstimationPreprocessor(Protocol):
 
 
 class EstimateExpectationValues(Protocol):
-    """Protocol defining function estimates expectation values for a list of estimation tasks."""
+    """Protocol defining function estimates expectation values for a list of estimation tasks.
+    Implementations of this protocol should obey the following rules:
+    1. Return one ExpectationValue for each EstimationTask.
+    2. Number of entries in each ExpectationValue is not restricted.
+    3. Output ExpectationValue should include coefficients of the terms/operators.
+    4. estimation_tasks can include tasks where operator consists of a constant term or contains a constant term,
+        it's responsibility of the implemented method to handle it properly.
+    """
 
     def __call__(
         self, backend: QuantumBackend, estimation_tasks: List[EstimationTask]

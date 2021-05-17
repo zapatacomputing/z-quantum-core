fe60aa414cb7f51750f05bb59d6581cb48915855
Max Radin <radin.max@gmail.com>
2021-05-17 12:02:20 -0400

    Last flake8 fixes for src

--- src/python/zquantum/core/circuit/_qubit.py
+++ src/python/zquantum/core/circuit/_qubit.py
@@ -94,11 +94,13 @@ class Qubit(object):
         output = cls(index)
         output.info["label"] = "cirq"
         if isinstance(cirq_qubit, cirq.GridQubit):
-            qubit_key = lambda q: (q.row, q.col)
+            def qubit_key(q):
+                return (q.row, q.col)
             output.info["QubitType"] = "GridQubit"
             output.info["QubitKey"] = qubit_key(cirq_qubit)
         elif isinstance(cirq_qubit, cirq.LineQubit):
-            qubit_key = lambda q: q.x
+            def qubit_key(q):
+                return q.x
             output.info["QubitType"] = "LineQubit"
             output.info["QubitKey"] = qubit_key(cirq_qubit)
         else:

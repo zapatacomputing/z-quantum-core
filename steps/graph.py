f6d686bae4fcb359130e59391c0d9a9c5b3521b6
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-05-20 11:31:27 +0200

    Run black on steps

--- steps/graph.py
+++ steps/graph.py
@@ -17,10 +17,10 @@ from zquantum.core.graph import save_graph
 
 
 def generate_random_graph_erdos_renyi(
-        number_of_nodes: int,
-        edge_probability: float,
-        random_weights: bool = False,
-        seed: Optional[int] = None,
+    number_of_nodes: int,
+    edge_probability: float,
+    random_weights: bool = False,
+    seed: Optional[int] = None,
 ):
     graph = _generate_random_graph_erdos_renyi(
         number_of_nodes, edge_probability, random_weights, seed
@@ -29,10 +29,10 @@ def generate_random_graph_erdos_renyi(
 
 
 def generate_random_regular_graph(
-        number_of_nodes: int,
-        degree: int,
-        random_weights: bool = False,
-        seed: Optional[int] = None,
+    number_of_nodes: int,
+    degree: int,
+    random_weights: bool = False,
+    seed: Optional[int] = None,
 ):
     graph = _generate_random_regular_graph(
         number_of_nodes, degree, random_weights, seed
@@ -41,7 +41,7 @@ def generate_random_regular_graph(
 
 
 def generate_complete_graph(
-        number_of_nodes: int, random_weights: bool = False, seed: Optional[int] = None
+    number_of_nodes: int, random_weights: bool = False, seed: Optional[int] = None
 ):
     graph = _generate_random_graph_erdos_renyi(
         number_of_nodes, 1.0, random_weights, seed
@@ -50,7 +50,10 @@ def generate_complete_graph(
 
 
 def generate_caveman_graph(
-        number_of_cliques: int, size_of_cliques: int, random_weights: bool = False, seed: Optional[int] = None
+    number_of_cliques: int,
+    size_of_cliques: int,
+    random_weights: bool = False,
+    seed: Optional[int] = None,
 ):
     graph = _generate_caveman_graph(
         number_of_cliques, size_of_cliques, random_weights, seed
@@ -59,17 +62,16 @@ def generate_caveman_graph(
 
 
 def generate_ladder_graph(
-        length_of_ladder: int, random_weights: bool = False, seed: Optional[int] = None
+    length_of_ladder: int, random_weights: bool = False, seed: Optional[int] = None
 ):
-    graph = _generate_ladder_graph(
-        length_of_ladder, random_weights, seed
-    )
+    graph = _generate_ladder_graph(length_of_ladder, random_weights, seed)
     save_graph(graph, "graph.json")
 
 
 def generate_barbell_graph(
-        number_of_vertices_complete_graph: int, random_weights: bool = False,
-        seed: Optional[int] = None
+    number_of_vertices_complete_graph: int,
+    random_weights: bool = False,
+    seed: Optional[int] = None,
 ):
     graph = _generate_barbell_graph(
         number_of_vertices_complete_graph, random_weights, seed

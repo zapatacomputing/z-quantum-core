b4fd2ad1702f79894def042abe0525f80a8501b0
Konrad Jałowiecki <dexter2206@gmail.com>
2021-06-08 11:58:40 +0200

    Add docstrings to Operation protocol

--- src/python/zquantum/core/circuits/_operations.py
+++ src/python/zquantum/core/circuits/_operations.py
@@ -12,13 +12,32 @@ class Operation(Protocol):
 
     @property
     def params(self) -> Tuple[Parameter, ...]:
+        """Parameters of this operation."""
         raise NotImplementedError()
 
     def bind(self: T, symbols_map: Dict[sympy.Symbol, Parameter]) -> T:
+        """Create new operation by replacing free symbols in operation params.
+
+        The operation returned by this method should be of the same type
+        as self, e.g. binding parameters to GateOperation should produce
+        GateOperation.
+        """
         raise NotImplementedError()
 
     def replace_params(self: T, new_params: Tuple[Parameter, ...]) -> T:
+        """Create new operation by replacing params.
+
+        The difference between bind and replace params is that it bind performs
+        parameter substitution - in particular, parameters without free symbols
+        are unaffected by bind, whereas replace_params replaces *all* params.
+        """
         raise NotImplementedError()
 
     def free_symbols(self):
+        """Free symbols parameterizing this operation.
+
+        Note that number of free_symbols is unrelated to number of params.
+        Some params can be expressions with multiple free symbols, while other params
+        might not comprise free symbols at all.
+        """
         raise NotImplementedError()

1db30c84d8a40a5a0a5a2dd26a39b756675d6e7c
Alexander Juda <6004040+alexjuda@users.noreply.github.com>
2021-06-08 11:52:53 +0200

    Compatibility with older sympy version (#356)
    * Loosen requirement for sympy
    
    * Make circuit conversions work with older sympy version

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setuptools.setup(
         "networkx==2.4",
         "numpy>=1.20",
         "scipy>=1.4.1",
-        "sympy>=1.7",
+        "sympy>=1.5",
         "openfermion>=1.0.0",
         "openfermioncirq==0.4.0",
         "lea>=3.2.0",

9a117ce5ee96484d8934ad83c2a5a4dffac516d1
Shiro-Raven <exclass9.24@gmail.com>
2021-08-05 22:17:04 +0200

    ZQS-359 added tests for estimator contracts after fixing some bugs

--- src/python/zquantum/core/interfaces/estimator_contract.py
+++ src/python/zquantum/core/interfaces/estimator_contract.py
@@ -16,16 +16,19 @@ Here is an example of how you would do that:
 import numpy as np
 from openfermion import IsingOperator
 from zquantum.core.circuits import RX, RY, RZ, Circuit, H
-from zquantum.core.interfaces.estimation import EstimationTask
+from zquantum.core.interfaces.estimation import (
+    EstimateExpectationValues,
+    EstimationTask,
+)
 from zquantum.core.symbolic_simulator import SymbolicSimulator
 
-backend = SymbolicSimulator(seed=1997)
+_backend = SymbolicSimulator(seed=1997)
 
-estimation_tasks = [
+_estimation_tasks = [
     EstimationTask(IsingOperator("Z0"), Circuit([H(0)]), 10000),
     EstimationTask(
         IsingOperator("Z0") + IsingOperator("Z1") + IsingOperator("Z2"),
-        Circuit([H(0), H(1), H(2)]),
+        Circuit([H(0), RX(np.pi / 3)(0), H(2)]),
         10000,
     ),
     EstimationTask(
@@ -43,50 +46,64 @@ estimation_tasks = [
 ]
 
 
-def _validate_each_task_returns_one_expecation_value(estimator):
+def _validate_each_task_returns_one_expecation_value(
+    estimator: EstimateExpectationValues,
+):
     # When
     expectation_values = estimator(
-        backend=backend,
-        estimation_tasks=estimation_tasks,
+        backend=_backend,
+        estimation_tasks=_estimation_tasks,
     )
 
     # Then
-    return len(expectation_values) == len(estimation_tasks)
+    return len(expectation_values) == len(_estimation_tasks)
 
 
-def _validate_order_of_outputs_matches_order_of_inputs(estimator):
+def _validate_order_of_outputs_matches_order_of_inputs(
+    estimator: EstimateExpectationValues,
+):
     expectation_values = estimator(
-        backend=backend,
-        estimation_tasks=estimation_tasks,
+        backend=_backend,
+        estimation_tasks=_estimation_tasks,
     )
 
     return all(
         [
-            expectation_values[i].values
-            == estimator(
-                backend=backend,
-                estimation_tasks=[task],
-            )[0].values
-            for i, task in enumerate(estimation_tasks)
+            np.array_equal(
+                expectation_values[i].values,
+                estimator(
+                    backend=_backend,
+                    estimation_tasks=[task],
+                )[0].values,
+            )
+            for i, task in enumerate(_estimation_tasks)
         ]
     )
 
 
-def _validate_expectation_value_includes_coefficients(estimator):
+def _validate_expectation_value_includes_coefficients(
+    estimator: EstimateExpectationValues,
+):
     estimation_tasks = [
-        EstimationTask(IsingOperator("Z0"), Circuit([H(0)]), 10000),
-        EstimationTask(IsingOperator("Z0", 19.971997), Circuit([H(0)]), 10000),
+        EstimationTask(IsingOperator("Z0"), Circuit([RX(np.pi / 3)(0)]), 10000),
+        EstimationTask(
+            IsingOperator("Z0", 19.971997), Circuit([RX(np.pi / 3)(0)]), 10000
+        ),
     ]
 
     expectation_values = estimator(
-        backend=backend,
+        backend=_backend,
         estimation_tasks=estimation_tasks,
     )
 
-    return expectation_values[0].values != expectation_values[1].values
+    return not np.array_equal(
+        expectation_values[0].values, expectation_values[1].values
+    )
 
 
-def _validate_constant_terms_are_included_in_output(estimator):
+def _validate_constant_terms_are_included_in_output(
+    estimator: EstimateExpectationValues,
+):
     estimation_tasks = [
         EstimationTask(IsingOperator("Z0"), Circuit([H(0)]), 10000),
         EstimationTask(
@@ -97,11 +114,13 @@ def _validate_constant_terms_are_included_in_output(estimator):
     ]
 
     expectation_values = estimator(
-        backend=backend,
+        backend=_backend,
         estimation_tasks=estimation_tasks,
     )
 
-    return expectation_values[0].values != expectation_values[1].values
+    return not np.array_equal(
+        expectation_values[0].values, expectation_values[1].values
+    )
 
 
 ESTIMATOR_CONTRACT = [

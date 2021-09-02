a5d78b8bd5e36379fbeafab1a0fd51c260cfeef4
Shiro-Raven <exclass9.24@gmail.com>
2021-09-02 15:22:33 +0200

    ZQS-757 modify translation tests and fix bug in sympy dialect

--- tests/zquantum/core/circuits/symbolic/translations_test.py
+++ tests/zquantum/core/circuits/symbolic/translations_test.py
@@ -16,16 +16,8 @@ from zquantum.core.circuits.symbolic.translations import translate_expression
         sympy.cos(sympy.Symbol("theta")),
         sympy.cos(2 * sympy.Symbol("theta")),
         sympy.exp(sympy.Symbol("x") - sympy.Symbol("y")),
-        sympy.Add(
-            sympy.cos(sympy.Symbol("phi")),
-            sympy.I * sympy.sin(sympy.Symbol("phi")),
-            evaluate=False,
-        ),
-        sympy.Add(
-            sympy.Symbol("x"),
-            sympy.Mul(sympy.Symbol("y"), (2 + 3j), evaluate=False),
-            evaluate=False,
-        ),
+        sympy.cos("phi") + sympy.I * sympy.sin(sympy.Symbol("phi")),
+        sympy.Symbol("x") + sympy.Symbol("y") * (2 + 3j),
         sympy.cos(sympy.sin(sympy.Symbol("tau"))),
         sympy.Symbol("x") / sympy.Symbol("y"),
         sympy.tan(sympy.Symbol("theta")),
@@ -33,7 +25,8 @@ from zquantum.core.circuits.symbolic.translations import translate_expression
         sympy.Symbol("y") ** sympy.Symbol("x"),
         sympy.Symbol("x") ** 2,
         sympy.sqrt(sympy.Symbol("x") - sympy.Symbol("y")),
-        sympy.Mul(-5, sympy.Mul(sympy.Symbol("x"), sympy.Symbol("y")), evaluate=False),
+        -5 * sympy.Symbol("x") * sympy.Symbol("y"),
+        sympy.Symbol("x") + sympy.Symbol("y") + 9,
     ],
 )
 def test_translating_tree_from_sympy_to_quil_gives_expected_result(sympy_expression):

e3e977efdb962de1f661f0bf33043f17bea8142d
Shiro-Raven <exclass9.24@gmail.com>
2021-09-03 13:50:29 +0200

    ZQS-759 adapt SymbolicSimulator to Wavefunction and make WF.bind() consistent with core

--- src/python/zquantum/core/wavefunction.py
+++ src/python/zquantum/core/wavefunction.py
@@ -5,11 +5,6 @@ import numpy as np
 from sympy import Abs, Matrix, Symbol
 
 
-def _calculate_probability_of_ground_entries(arr: Matrix) -> np.float64:
-    numbers = np.array([elem for elem in arr if elem.is_number], dtype=np.complex128)
-    return np.sum(np.abs(numbers) ** 2)
-
-
 class Wavefunction:
     """
     A simple wavefunction data structure that can
@@ -39,7 +34,10 @@ class Wavefunction:
 
     @property
     def amplitudes(self):
-        return np.array(self._wavefunction, dtype=np.complex128).flatten()
+        try:
+            return np.array(self._wavefunction, dtype=np.complex128).flatten()
+        except TypeError:
+            return np.array(self._wavefunction, dtype=object).flatten()
 
     @staticmethod
     def _check_sanity(arr: Matrix):
@@ -48,6 +46,13 @@ class Wavefunction:
         #1 no free symbols --> must ensure unit probability
         #2 free symbols --> must ensure numbers do not exceed unit probability already
         """
+
+        def _calculate_probability_of_ground_entries(arr: Matrix) -> np.float64:
+            numbers = np.array(
+                [elem for elem in arr if elem.is_number], dtype=np.complex128
+            )
+            return np.sum(np.abs(numbers) ** 2)
+
         probs_of_ground_entries = _calculate_probability_of_ground_entries(arr)
         if len(arr.free_symbols) == 0:
             if not np.isclose(probs_of_ground_entries, 1.0):
@@ -62,15 +67,7 @@ class Wavefunction:
         return len(self._wavefunction)
 
     def __iter__(self):
-        self._i = 0
         return iter(self._wavefunction)
-
-    def __next__(self):
-        if self._i < len(self):
-            self._i += 1
-            return self[self._i]
-
-        raise StopIteration
 
     def __getitem__(self, idx):
         return self._wavefunction[idx]
@@ -95,18 +92,13 @@ class Wavefunction:
         np_arr[0] = 1.0
         return Wavefunction(np_arr)
 
-    def bind(self, symbol_map: Dict[Symbol, Any], overwrite_symbols=True) -> Matrix:
+    def bind(self, symbol_map: Dict[Symbol, Any]) -> "Wavefunction":
         result = self._wavefunction.subs(symbol_map)
 
         try:
-            self._check_sanity(result)
+            return type(self)(result)
         except ValueError:
             raise ValueError("Passed map results in a violation of probability unity.")
-
-        if overwrite_symbols:
-            self._wavefunction = result
-
-        return result
 
     def probabilities(self) -> Matrix:
         absoluted_wf = Abs(self._wavefunction)
--- tests/zquantum/core/symbolic_simulator_test.py
+++ tests/zquantum/core/symbolic_simulator_test.py
@@ -19,13 +19,6 @@ def wf_simulator():
 
 
 class TestSymbolicSimulator(QuantumSimulatorTests):
-    def test_get_wavefunction_raises_if_circuit_contains_free_symbols(
-        self, wf_simulator
-    ):
-        circuit = circuits.Circuit([circuits.RX(sympy.Symbol("theta"))(2)])
-        with pytest.raises(ValueError):
-            wf_simulator.get_wavefunction(circuit)
-
     def test_cannot_sample_from_circuit_containing_free_symbols(self, wf_simulator):
         circuit = circuits.Circuit([circuits.XX(sympy.Symbol("theta"))(2, 1)])
         with pytest.raises(ValueError):

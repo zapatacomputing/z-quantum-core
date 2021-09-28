16d58dd21936ae60391719a4b901c180de4e4350
Konrad Jałowiecki <dexter2206@gmail.com>
2021-09-28 13:10:41 +0200

    ZQS-257 Export Operation from zquantum.core.circuits package

--- src/python/zquantum/core/circuits/__init__.py
+++ src/python/zquantum/core/circuits/__init__.py
@@ -144,6 +144,7 @@ from ._gates import (
     MatrixFactoryGate,
 )
 from ._generators import add_ancilla_register, create_layer_of_gates
+from ._operations import Operation
 from ._serde import (
     circuit_from_dict,
     circuitset_from_dict,

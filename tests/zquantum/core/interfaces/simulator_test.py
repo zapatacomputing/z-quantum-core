50fc78abff07dc36fd6ed09017fe589b82325387
Konrad Jałowiecki <dexter2206@gmail.com>
2021-10-04 15:42:36 +0200

    ZQS-257 Add test that by default only GateOperations are natively supported

--- tests/zquantum/core/interfaces/simulator_test.py
+++ tests/zquantum/core/interfaces/simulator_test.py
@@ -1,12 +1,18 @@
 import numpy as np
 import pytest
-from zquantum.core.circuits import CNOT, RX, RY, Circuit, Operation
+from zquantum.core.circuits import CNOT, RX, RY, Circuit, MultiPhaseOperation, Operation
+from zquantum.core.interfaces.backend import QuantumSimulator
 from zquantum.core.symbolic_simulator import SymbolicSimulator
 
 
 class SymbolicSimulatorWithNonSupportedOperations(SymbolicSimulator):
     def is_natively_supported(self, operation: Operation) -> bool:
         return super().is_natively_supported(operation) and operation.gate.name != "RX"
+
+
+class SymbolicSimulatorWithDefaultSetOfSupportedOperations(SymbolicSimulator):
+    def is_natively_supported(self, operation: Operation) -> bool:
+        return QuantumSimulator.is_natively_supported(self, operation)
 
 
 @pytest.mark.parametrize(
@@ -26,4 +32,12 @@ def test_quantum_simulator_switches_between_native_and_nonnative_modes_of_execut
     np.testing.assert_array_equal(
         simulator.get_wavefunction(circuit).amplitudes,
         reference_simulator.get_wavefunction(circuit).amplitudes,
+    )
+
+
+def test_by_default_only_gate_operations_are_supported():
+    simulator = SymbolicSimulatorWithDefaultSetOfSupportedOperations()
+    assert simulator.is_natively_supported(RX(np.pi / 2)(1))
+    assert not simulator.is_natively_supported(
+        MultiPhaseOperation((0.5, 0.2, 0.3, 0.1))
     )

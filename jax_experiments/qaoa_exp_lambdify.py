5a547129685e9b411e5faf2cb8c4dc8e5b949757
Shiro-Raven <exclass9.24@gmail.com>
2021-11-01 22:13:27 +0100

    add file for non-working lambdify experiment

--- jax_experiments/qaoa_exp_lambdify.py
+++ jax_experiments/qaoa_exp_lambdify.py
@@ -1,9 +1,8 @@
 import networkx as nx
 import sympy
-from jax import grad, jit, random
+from jax import grad, random
 from numpy import argpartition
 from openfermion.ops.operators.ising_operator import IsingOperator
-from sympy2jax import sympy2jax
 from zquantum.core.estimation import calculate_exact_expectation_values
 from zquantum.core.interfaces.estimation import EstimationTask
 from zquantum.core.interfaces.functions import function_with_gradient
@@ -12,8 +11,58 @@ from zquantum.core.symbolic_simulator import SymbolicSimulator
 from zquantum.optimizers.simple_gradient_descent import SimpleGradientDescent
 from zquantum.qaoa.ansatzes.farhi_ansatz import QAOAFarhiAnsatz
 from zquantum.qaoa.problems.maxcut import MaxCut
+import jax.numpy as jnp
+from jax.scipy import special as jsp
 
 sim = SymbolicSimulator()
+
+"""
+WARNING: THIS CODE IS STILL NON-FUNCTIONING
+"""
+
+
+_jnp_func_lookup = {
+    "sympy.Mul": jnp.multiply,
+    "sympy.Add": jnp.add,
+    "sympy.div": jnp.divide,
+    "sympy.Abs": jnp.abs,
+    "sympy.sign": jnp.sign,
+    # Note: May raise error for ints.
+    "sympy.ceiling": jnp.ceil,
+    "sympy.floor": jnp.floor,
+    "sympy.log": jnp.log,
+    "sympy.exp": jnp.exp,
+    "sympy.sqrt": jnp.sqrt,
+    "cos": jnp.cos,
+    "sympy.acos": jnp.arccos,
+    "sympy.sin": jnp.sin,
+    "sympy.asin": jnp.arcsin,
+    "sympy.tan": jnp.tan,
+    "sympy.atan": jnp.arctan,
+    "sympy.atan2": jnp.arctan2,
+    # Note: Also may give NaN for complex results.
+    "sympy.cosh": jnp.cosh,
+    "sympy.acosh": jnp.arccosh,
+    "sympy.sinh": jnp.sinh,
+    "sympy.asinh": jnp.arcsinh,
+    "sympy.tanh": jnp.tanh,
+    "sympy.atanh": jnp.arctanh,
+    "sympy.Pow": jnp.power,
+    "sympy.re": jnp.real,
+    "sympy.im": jnp.imag,
+    # Note: May raise error for ints and complexes
+    "sympy.erf": jsp.erf,
+    "sympy.erfc": jsp.erfc,
+    "sympy.LessThan": jnp.less,
+    "sympy.GreaterThan": jnp.greater,
+    "sympy.And": jnp.logical_and,
+    "sympy.Or": jnp.logical_or,
+    "sympy.Not": jnp.logical_not,
+    "sympy.Max": jnp.fmax,
+    "sympy.Min": jnp.fmin,
+    "sympy.Mod": jnp.fmod,
+    "sympy.conjugate": jnp.conjugate,
+}
 
 
 def exp_qaoa():
@@ -41,26 +90,25 @@ print("Ran circuit...")
 
 symbolic_exp = sympy.re(sympy.simplify(symbolic_exp))
 print("Simplified and Real-ized symbolic expectation expression...")
+import inspect
 
-f, params = sympy2jax(symbolic_exp, list(symbolic_exp.free_symbols))
-
-lamb_f = lambda x, y: f(x, y)[0]
-compiled_f = jit(lamb_f)
-compiled_f_grad = grad(compiled_f)
-
-compiled_f_with_params = lambda x: compiled_f(x, params)
-compiled_f_grad_with_params = lambda x: compiled_f_grad(x, params)
+fun = sympy.lambdify(
+    list(symbolic_exp.free_symbols), symbolic_exp, modules=_jnp_func_lookup
+)
+lamb_f = lambda x: fun(*x[0])
+key = random.PRNGKey(0)
+X = random.normal(key, (1, len(symbolic_exp.free_symbols)))
+lines = inspect.getsource(fun)
+breakpoint()
 print("Transformed to JAX function...")
 
-fun = function_with_gradient(compiled_f_with_params, compiled_f_grad_with_params)
+fun = function_with_gradient(lamb_f, grad(lamb_f))
 
 grad_desc = SimpleGradientDescent(
     0.1,
     100,
 )
 
-key = random.PRNGKey(0)
-X = random.normal(key, (1, len(symbolic_exp.free_symbols)))
 print("Prepared for optimization...")
 
 res = grad_desc.minimize(fun, X, keep_history=False)

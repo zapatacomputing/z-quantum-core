f7564535213155940b451dd93748cec6dd743cfe
Philip Carinhas <pac@fortuitous.com>
2021-11-11 04:08:03 +0000

    wip2

--- tests/zquantum/conftest.py
+++ tests/zquantum/conftest.py
@@ -4,7 +4,6 @@ import pytest
 def pytest_collection_modifyitems(config, items):
     """Mark every test file in this folder as unit"""
     for item in items:
-        mark_name = "unit"
-        if mark_name:
-            mark = getattr(pytest.mark, mark_name)
+        mark = getattr(pytest.mark, "unit")
+        if item.name.endswith("_test"):
             item.add_marker(mark)

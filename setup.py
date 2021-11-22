b8a99238f4b9338104ab0da85d6e69e211e02e64
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-11-22 15:13:42 +0100

    ORQSDK-166 Pin openfermion to a specific commit

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
         "numpy>=1.20",
         "scipy>=1.4.1",
         "sympy>=1.5",
-        "openfermion @ git+https://github.com/quantumlib/OpenFermion",
+        "openfermion @ git+https://github.com/quantumlib/OpenFermion@7532ad9e6d34f6922537f00a5cc001047f2bfdd1",  # noqa: 501
         "lea>=3.2.0",
         "overrides~=3.1",
         "python-rapidjson",

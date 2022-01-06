20f6c15361cf1cd22ed961108dfe3eed05f7bc51
AthenaCaesura <athena.caesura@zapatacomputing.com>
2022-01-06 16:34:13 -0500

    Added XtoRZRY decomposition

--- src/python/zquantum/core/wip/decompositions/_ryrzcnot_decompositions.py
+++ src/python/zquantum/core/wip/decompositions/_ryrzcnot_decompositions.py
@@ -1,16 +1,13 @@
 from typing import Iterable
 
 import numpy as np
-from zquantum.core.circuits._builtin_gates import RY, RZ
+from zquantum.core.circuits._builtin_gates import GPHASE, RY, RZ
 from zquantum.core.circuits._gates import GateOperation
 from zquantum.core.decompositions._decomposition import DecompositionRule
 
 
 class RXtoRZRY(DecompositionRule[GateOperation]):
-    """Decomposition of ZQuantum's RX gate in the RZRYCNOT gateset.
-
-    Note that this gets rid of global phase.
-    """
+    """Decomposition of ZQuantum's RX gate in the RZRYCNOT gateset."""
 
     def predicate(self, operation: GateOperation) -> bool:
         # Only decompose RX to RY and RZ
@@ -20,4 +17,27 @@ class RXtoRZRY(DecompositionRule[GateOperation]):
         lambda_ = operation.params[0]
         indices = operation.qubit_indices[0]
 
-        return [RZ(np.pi / 2)(indices), RY(lambda_)(indices), RZ(-np.pi / 2)(indices)]
+        return [
+            RZ(np.pi / 2)(indices),
+            RY(lambda_)(indices),
+            RZ(-np.pi / 2)(indices),
+            GPHASE(-np.pi / 2)(indices),
+        ]
+
+
+class XtoRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's X gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose RX to RY and RZ
+        return operation.gate.name == "X"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        indices = operation.qubit_indices[0]
+
+        return [
+            RZ(np.pi / 2)(indices),
+            RY(np.pi / 2)(indices),
+            RZ(-np.pi / 2)(indices),
+            GPHASE(-np.pi / 2)(indices),
+        ]

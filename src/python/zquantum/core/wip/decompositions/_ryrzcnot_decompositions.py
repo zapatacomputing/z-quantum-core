978bf67dc34b5769fc23a5b341c03f818505b643
Ntwali B <toussaint@zapatacomputing.com>
2022-01-14 14:05:22 +0100

    Finished the implementation and testing of all single qubit gates decompositions in the {GPHASE, RY, RZ, CNOT} gateset.

--- src/python/zquantum/core/wip/decompositions/_ryrzcnot_decompositions.py
+++ src/python/zquantum/core/wip/decompositions/_ryrzcnot_decompositions.py
@@ -6,6 +6,23 @@ from zquantum.core.circuits._gates import GateOperation
 from zquantum.core.decompositions._decomposition import DecompositionRule
 
 
+class PHASEtoRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's PHASE gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose PHASE to RY and RZ
+        return operation.gate.name == "PHASE"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        alpha = operation.params[0]
+        indices = operation.qubit_indices[0]
+
+        return [
+            RZ(alpha)(indices),
+            GPHASE(alpha / 2)(indices),
+        ]
+
+
 class RXtoRZRY(DecompositionRule[GateOperation]):
     """Decomposition of ZQuantum's RX gate in the RZRYCNOT gateset."""
 
@@ -14,13 +31,51 @@ class RXtoRZRY(DecompositionRule[GateOperation]):
         return operation.gate.name == "RX"
 
     def production(self, operation: GateOperation) -> Iterable[GateOperation]:
-        lambda_ = operation.params[0]
+        theta = operation.params[0]
         indices = operation.qubit_indices[0]
 
         return [
             RZ(-np.pi/2)(indices),
-            RY(lambda_)(indices),
+            RY(theta)(indices),
             RZ(np.pi/2)(indices),
+        ]
+
+
+class U3toRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's U3 gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose U3 to RY and RZ
+        return operation.gate.name == "U3"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        phi = operation.params[0]
+        theta = operation.params[1]
+        lambda_ = operation.params[2]
+        indices = operation.qubit_indices[0]
+
+        return [
+            RZ(phi)(indices),
+            RY(theta)(indices),
+            RZ(lambda_)(indices),
+            GPHASE((phi + lambda_) / 2)(indices),
+        ]
+
+
+class ItoRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's I gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose I to RY and RZ
+        return operation.gate.name == "I"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        indices = operation.qubit_indices[0]
+
+        return [
+            RZ(0)(indices),
+            RY(0)(indices),
+            RZ(0)(indices),
         ]
 
 
@@ -28,7 +83,7 @@ class XtoRZRY(DecompositionRule[GateOperation]):
     """Decomposition of ZQuantum's X gate in the RZRYCNOT gateset."""
 
     def predicate(self, operation: GateOperation) -> bool:
-        # Only decompose RX to RY and RZ
+        # Only decompose X to RY and RZ
         return operation.gate.name == "X"
 
     def production(self, operation: GateOperation) -> Iterable[GateOperation]:
@@ -39,4 +94,85 @@ class XtoRZRY(DecompositionRule[GateOperation]):
             RZ(-np.pi/2)(indices),
             RY(np.pi)(indices),
             RZ(np.pi/2)(indices),
+        ]
+
+
+class YtoRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's Y gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose Y to RY and RZ
+        return operation.gate.name == "Y"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        indices = operation.qubit_indices[0]
+
+        return [
+            GPHASE(np.pi/2)(indices),
+            RY(np.pi)(indices),
+        ]
+
+
+class ZtoRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's Z gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose Z to RY and RZ
+        return operation.gate.name == "Z"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        indices = operation.qubit_indices[0]
+
+        return [
+            GPHASE(np.pi/2)(indices),
+            RZ(np.pi)(indices),
+        ]
+
+
+class HtoRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's H gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose H to RY and RZ
+        return operation.gate.name == "H"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        indices = operation.qubit_indices[0]
+
+        return [
+            RY(np.pi/2)(indices),
+            GPHASE(np.pi/2)(indices),
+            RZ(np.pi)(indices),
+        ]
+
+
+class StoRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's S gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose S to RY and RZ
+        return operation.gate.name == "S"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        indices = operation.qubit_indices[0]
+
+        return [
+            RZ(np.pi / 2)(indices),
+            GPHASE(np.pi / 4)(indices),
+        ]
+
+
+class TtoRZRY(DecompositionRule[GateOperation]):
+    """Decomposition of ZQuantum's T gate in the RZRYCNOT gateset."""
+
+    def predicate(self, operation: GateOperation) -> bool:
+        # Only decompose T to RY and RZ
+        return operation.gate.name == "T"
+
+    def production(self, operation: GateOperation) -> Iterable[GateOperation]:
+        indices = operation.qubit_indices[0]
+
+        return [
+            RZ(np.pi / 4)(indices),
+            GPHASE(np.pi / 8)(indices),
         ]
--- tests/zquantum/core/wip/decompositions/_ryrzcnot_decompositions_test.py
+++ tests/zquantum/core/wip/decompositions/_ryrzcnot_decompositions_test.py
@@ -1,8 +1,27 @@
 import unittest
 
 import numpy as np
-from zquantum.core.circuits import GPHASE, RX, RY, RZ, X
-from zquantum.core.wip.decompositions._ryrzcnot_decompositions import RXtoRZRY, XtoRZRY
+from zquantum.core.circuits import GPHASE, PHASE, RX, RY, RZ, U3, I, X, Y, Z, H, S, T
+from zquantum.core.wip.decompositions._ryrzcnot_decompositions import PHASEtoRZRY, RXtoRZRY, U3toRZRY, ItoRZRY, XtoRZRY, YtoRZRY, ZtoRZRY, HtoRZRY, StoRZRY, TtoRZRY
+
+
+class TestDecompositionPHASEIntoRYRZCNOT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.decomp_rule = PHASEtoRZRY()
+        self.operation_to_decompose = PHASE(np.pi)(2)
+        self.targets = [
+            RZ(np.pi)(2),
+            GPHASE(np.pi / 2)(2),
+        ]
+
+    def test_predicate(self):
+        assert self.decomp_rule.predicate(self.operation_to_decompose)
+
+    def test_production(self):
+        prod = self.decomp_rule.production(self.operation_to_decompose)
+
+        for operation, target in zip(prod, self.targets):
+            self.assertEqual(operation, target)
 
 
 class TestDecompositionRXIntoRYRZCNOT(unittest.TestCase):
@@ -25,6 +44,47 @@ class TestDecompositionRXIntoRYRZCNOT(unittest.TestCase):
             self.assertEqual(operation, target)
 
 
+class TestDecompositionU3IntoRYRZCNOT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.decomp_rule = U3toRZRY()
+        self.operation_to_decompose = U3(0.1, 0.2, 0.3)(2)
+        self.targets = [
+            RZ(0.1)(2),
+            RY(0.2)(2),
+            RZ(0.3)(2),
+            GPHASE(0.2)(2),
+        ]
+
+    def test_predicate(self):
+        assert self.decomp_rule.predicate(self.operation_to_decompose)
+
+    def test_production(self):
+        prod = self.decomp_rule.production(self.operation_to_decompose)
+
+        for operation, target in zip(prod, self.targets):
+            self.assertEqual(operation, target)
+
+
+class TestDecompositionIIntoRYRZCNOT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.decomp_rule = ItoRZRY()
+        self.operation_to_decompose = I(2)
+        self.targets = [
+            RZ(0)(2),
+            RY(0)(2),
+            RZ(0)(2),
+        ]
+
+    def test_predicate(self):
+        assert self.decomp_rule.predicate(self.operation_to_decompose)
+
+    def test_production(self):
+        prod = self.decomp_rule.production(self.operation_to_decompose)
+
+        for operation, target in zip(prod, self.targets):
+            self.assertEqual(operation, target)
+
+
 class TestDecompositionXIntoRYRZCNOT(unittest.TestCase):
     def setUp(self) -> None:
         self.decomp_rule = XtoRZRY()
@@ -34,6 +94,102 @@ class TestDecompositionXIntoRYRZCNOT(unittest.TestCase):
             RZ(-np.pi / 2)(2),
             RY(np.pi)(2),
             RZ(np.pi / 2)(2),
+        ]
+
+    def test_predicate(self):
+        assert self.decomp_rule.predicate(self.operation_to_decompose)
+
+    def test_production(self):
+        prod = self.decomp_rule.production(self.operation_to_decompose)
+
+        for operation, target in zip(prod, self.targets):
+            self.assertEqual(operation, target)
+
+
+class TestDecompositionYIntoRYRZCNOT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.decomp_rule = YtoRZRY()
+        self.operation_to_decompose = Y(2)
+        self.targets = [
+            GPHASE(np.pi/2)(2),
+            RY(np.pi)(2),
+        ]
+
+    def test_predicate(self):
+        assert self.decomp_rule.predicate(self.operation_to_decompose)
+
+    def test_production(self):
+        prod = self.decomp_rule.production(self.operation_to_decompose)
+
+        for operation, target in zip(prod, self.targets):
+            self.assertEqual(operation, target)
+
+
+class TestDecompositionZIntoRYRZCNOT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.decomp_rule = ZtoRZRY()
+        self.operation_to_decompose = Z(2)
+        self.targets = [
+            GPHASE(np.pi/2)(2),
+            RZ(np.pi)(2),
+        ]
+
+    def test_predicate(self):
+        assert self.decomp_rule.predicate(self.operation_to_decompose)
+
+    def test_production(self):
+        prod = self.decomp_rule.production(self.operation_to_decompose)
+
+        for operation, target in zip(prod, self.targets):
+            self.assertEqual(operation, target)
+
+
+class TestDecompositionHIntoRYRZCNOT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.decomp_rule = HtoRZRY()
+        self.operation_to_decompose = H(2)
+        self.targets = [
+            RY(np.pi/2)(2),
+            GPHASE(np.pi/2)(2),
+            RZ(np.pi)(2),
+        ]
+
+    def test_predicate(self):
+        assert self.decomp_rule.predicate(self.operation_to_decompose)
+
+    def test_production(self):
+        prod = self.decomp_rule.production(self.operation_to_decompose)
+
+        for operation, target in zip(prod, self.targets):
+            self.assertEqual(operation, target)
+
+
+class TestDecompositionSIntoRYRZCNOT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.decomp_rule = StoRZRY()
+        self.operation_to_decompose = S(2)
+        self.targets = [
+            RZ(np.pi / 2)(2),
+            GPHASE(np.pi / 4)(2),
+        ]
+
+    def test_predicate(self):
+        assert self.decomp_rule.predicate(self.operation_to_decompose)
+
+    def test_production(self):
+        prod = self.decomp_rule.production(self.operation_to_decompose)
+
+        for operation, target in zip(prod, self.targets):
+            self.assertEqual(operation, target)
+
+
+class TestDecompositionTIntoRYRZCNOT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.decomp_rule = TtoRZRY()
+        self.operation_to_decompose = T(2)
+        self.targets = [
+            RZ(np.pi / 4)(2),
+            GPHASE(np.pi / 8)(2),
         ]
 
     def test_predicate(self):
